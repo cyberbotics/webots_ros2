c6ed178426fd83379bd94c05da9dc7bd3b3058e4
DavidMansolino <David.Mansolino@cyberbotics.com>
2019-07-25 17:31:17 +0200

    Set header.

--- webots_ros2/joint_state_publisher.py
+++ webots_ros2/joint_state_publisher.py
@@ -15,6 +15,7 @@
 """Joint state publisher."""
 
 from sensor_msgs.msg import JointState
+from builtin_interfaces.msg import Time
 from controller import Node
 
 
@@ -25,6 +26,7 @@ class JointStatePublisher(object):
         """Initialize the position sensors and the topic."""
         self.robot = robot
         self.jointPrefix = jointPrefix
+        self.node = node
         self.sensors = []
         self.timestep = int(robot.getBasicTimeStep())
         self.last_joint_states = None
@@ -41,12 +43,17 @@ class JointStatePublisher(object):
                 self.sensors.append(device)
                 self.previousPosition.append(0)
                 device.enable(self.timestep)
-        self.publisher = node.create_publisher(JointState, 'joint_states', 1)
+        self.publisher = self.node.create_publisher(JointState, 'joint_states', 1)
 
     def publish(self):
         """Publish the 'joint_states' topic with up to date value."""
+        #now = self.node.get_clock().now().nanoseconds
+        #seconds = int(now * 1.0e-6)
+        #nanoseconds = now - seconds
+        seconds = int(self.robot.getTime())
+        nanoseconds = int((self.robot.getTime() - seconds) * 1.0e+6)
         msg = JointState()
-        #msg.header.stamp = rospy.get_rostime()
+        msg.header.stamp = Time(sec=seconds, nanosec=nanoseconds)
         msg.header.frame_id = "From simulation state data"
         msg.name = [s + self.jointPrefix for s in self.jointNames]
         msg.position = []

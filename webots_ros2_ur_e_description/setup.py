8b0a7645c40767373b31a7da92e80fc1ca91b41b
DavidMansolino <David.Mansolino@cyberbotics.com>
2019-09-10 17:14:26 +0200

    Fix missing ament index.

--- webots_ros2_ur_e_description/setup.py
+++ webots_ros2_ur_e_description/setup.py
@@ -24,6 +24,7 @@ for rootPath, dirNames, fileNames in os.walk('urdf'):
         filePath = os.path.relpath(os.path.join(rootPath, fileName))
         data_files.append(('share/' + package_name + '/' + os.path.dirname(filePath), [filePath]))
 # Other files
+data_files.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
 data_files.append(('share/' + package_name, ['launch/ur5e_state_publisher.launch.py']))
 data_files.append(('share/' + package_name + '/rviz', ['rviz/view_robot.rviz']))
 data_files.append(('share/' + package_name, ['package.xml']))

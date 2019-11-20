e5cd46bdc69d950f2b18ae15d8e3905a1b9ac453
DavidMansolino <David.Mansolino@cyberbotics.com>
2019-11-20 12:29:48 +0100

    cleanup.

--- webots_ros2_demos/setup.py
+++ webots_ros2_demos/setup.py
@@ -1,11 +1,19 @@
 """webots_ros2 package setup file."""
 
+import os
+import fnmatch
+
 from setuptools import setup
 
 package_name = 'webots_ros2_demos'
 worlds = [
     'worlds/armed_robots.wbt'
 ]
+textures = []
+for rootPath, dirNames, fileNames in os.walk('worlds/textures'):
+    for fileName in fnmatch.filter(fileNames, '*.jpg'):
+        filePath = os.path.relpath(os.path.join(rootPath, fileName))
+        textures.append(filePath)
 launchers = [
     'launch/armed_robots.launch.py'
 ]
@@ -14,6 +22,7 @@ data_files = []
 data_files.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
 data_files.append(('share/' + package_name, launchers))
 data_files.append(('share/' + package_name + '/worlds', worlds))
+data_files.append(('share/' + package_name + '/worlds/textures', textures))
 data_files.append(('share/' + package_name, ['package.xml']))
 
 

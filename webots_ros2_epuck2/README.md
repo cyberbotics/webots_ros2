f3ffb41fa5d784ce08a274b5541ea1acfefc21c0
Darko Lukic <lukicdarkoo@gmail.com>
2020-02-18 18:17:25 +0100

    Add motors and camera

--- webots_ros2_epuck2/README.md
+++ webots_ros2_epuck2/README.md
@@ -2,3 +2,20 @@
 ```
 ros2 launch webots_ros2_epuck2 example.launch.py
 ```
+
+Read data from IR sensors:
+```
+ros2 topic echo /distance/ps6
+```
+
+Set speed:
+```
+ros2 topic pub /cmd_vel geometry_msgs/Twist "linear:
+  x: 0.1
+  y: 0.0
+  z: 0.0
+angular:
+  x: 0.0
+  y: 0.0
+  z: 0.0"
+```
--- webots_ros2_epuck2/webots_ros2_epuck2/epuck2_driver.py
+++ webots_ros2_epuck2/webots_ros2_epuck2/epuck2_driver.py
@@ -12,36 +12,74 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""ROS2 example controller."""
+"""ROS2 EPuck2 controller."""
 
 from webots_ros2_core.webots_node import WebotsNode
 import rclpy
-from sensor_msgs.msg import Range
+from sensor_msgs.msg import Range, Image
+from geometry_msgs.msg import Twist
 
+
+WHEEL_DISTANCE = 0.052
+WHEEL_RADIUS = 0.0205
+CAMERA_PERIOD_MS = 500
 
 
 class EPuck2Controller(WebotsNode):
     def __init__(self, args):
         super().__init__('epuck2_controller', args)
+
+        # Initialize motors
+        self.left_motor = self.robot.getMotor('left wheel motor')
+        self.right_motor = self.robot.getMotor('right wheel motor')
+        self.left_motor.setPosition(float('inf'))
+        self.right_motor.setPosition(float('inf'))
+        self.left_motor.setVelocity(0)
+        self.right_motor.setVelocity(0)
+        self.create_subscription(Twist, '/cmd_vel', self.cmd_vel_callback, 10)
+
+        # Intialize distance sensors
         self.sensor_publishers = []
         self.sensors = []
-        self.create_timer(0.001 * self.timestep, self.sensor_callback)
-
         for i in range(8):
             sensor = self.robot.getDistanceSensor('ps{}'.format(i))
             sensor.enable(self.timestep)
-            sensor_publisher = self.create_publisher(Range, 'sensor/ps{}'.format(i), 10)
+            sensor_publisher = self.create_publisher(
+                Range, '/distance/ps{}'.format(i), 10)
             self.sensors.append(sensor)
             self.sensor_publishers.append(sensor_publisher)
-            
-    def sensor_callback(self):
+        self.create_timer(0.01 * self.timestep, self.distance_callback)
+
+        # Initialize camera
+        self.camera = self.robot.getCamera('camera')
+        self.camera.enable(CAMERA_PERIOD_MS)
+        self.camera_publisher = self.create_publisher(Image, '/camera/image', 10)
+        self.create_timer(0.01 * self.timestep, self.camera_callback)
+
+    def camera_callback(self):
+        msg = Image()
+        msg.height = self.camera.getWidth()
+        msg.width = self.camera.getHeight()
+        msg.is_bigendian = False
+        msg.step = self.camera.getWidth() * 3
+        msg.data = self.camera.getImage()
+
+    def cmd_vel_callback(self, twist):
+        left_velocity = (2.0 * twist.linear.x - twist.angular.z *
+                         WHEEL_DISTANCE) / (2.0 * WHEEL_RADIUS)
+        right_velocity = (2.0 * twist.linear.x + twist.angular.z *
+                          WHEEL_DISTANCE) / (2.0 * WHEEL_RADIUS)
+        self.left_motor.setVelocity(left_velocity)
+        self.right_motor.setVelocity(right_velocity)
+
+    def distance_callback(self):
         for i in range(8):
             msg = Range()
-            msg.field_of_view = 0.01
-            msg.min_range = 0.1         # TODO: Take from simulation
-            msg.max_range = 10.0        # TODO: Take from simulation
+            msg.field_of_view = self.sensors[i].getAperture()
+            msg.min_range = self.sensors[i].getMinValue()
+            msg.max_range = self.sensors[i].getMaxValue()
             msg.range = self.sensors[i].getValue()
-            msg.radiation_type = Range.ULTRASOUND
+            msg.radiation_type = Range.INFRARED
             self.sensor_publishers[i].publish(msg)
 
 

4ba7b29d3a647204718539d4b80769e6701cec2e
Darko Lukic <lukicdarkoo@gmail.com>
2020-04-03 10:30:00 +0200

    Fix linter errors

--- webots_ros2_core/webots_ros2_core/differential_drive_node.py
+++ webots_ros2_core/webots_ros2_core/differential_drive_node.py
@@ -1,11 +1,23 @@
-from math import pi, cos, sin
-import rclpy
+# Copyright 1996-2020 Cyberbotics Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from math import cos, sin
 from webots_ros2_core.webots_node import WebotsNode
 from rcl_interfaces.msg import SetParametersResult
 from nav_msgs.msg import Odometry
 from webots_ros2_core.math_utils import euler_to_quaternion
 from geometry_msgs.msg import Twist, TransformStamped
-from webots_ros2_core.webots_node import WebotsNode
 from tf2_ros import TransformBroadcaster
 
 

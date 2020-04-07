7bacff65ff5ef57d5fcac02370fa9bc3b3edc3f1
Darko Lukic <lukicdarkoo@gmail.com>
2020-04-07 18:21:28 +0200

    Change configuration

--- webots_ros2_epuck/webots_ros2_epuck/simple_mapper.py
+++ webots_ros2_epuck/webots_ros2_epuck/simple_mapper.py
@@ -1,3 +1,4 @@
+import numpy
 import rclpy
 from rclpy.node import Node
 from nav_msgs.msg import OccupancyGrid
@@ -6,17 +7,35 @@ from geometry_msgs.msg import TransformStamped
 from tf2_ros import StaticTransformBroadcaster, TransformBroadcaster
 
 
-# WIP: python3 simple_mapper.py --ros-args --param use_sim_time:=true
+# [ERROR] [controller_server]: No valid trajectories out of 0!
+# python3 simple_mapper.py --ros-args --param use_sim_time:=true
+# ros2 launch nav2_bringup nav2_navigation_launch.py params_file:=/home/lukic/ros2_tooling/src/webots_ros2/webots_ros2_epuck/resource/nav2_params.yaml
+# RViz may not show cost map: https://github.com/ros-planning/navigation2/issues/921
+# https://answers.ros.org/question/286221/create-2d-occupancy-grid-map-by-laser-data/
+
+"""
+ros2 topic pub -1 /goal_pose geometry_msgs/PoseStamped \ '
+pose:
+  position:
+    x: 0.3
+    y: 0.0
+    z: 0.0
+  orientation:
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+'
+"""
 
 
 class SimpleMapper(Node):
     def __init__(self, name, args=None):
         super().__init__(name)
 
-        self.create_timer(0.1, self.main_loop)
+        self.create_timer(1, self.main_loop)
         self.map_publisher = self.create_publisher(OccupancyGrid, '/map', 1)
         self.tf_publisher = TransformBroadcaster(self)
-        self.static_tf_publisher = StaticTransformBroadcaster(self)
 
     def main_loop(self):
         now = self.get_clock().now()
@@ -25,25 +44,19 @@ class SimpleMapper(Node):
         tf.header.stamp = now.to_msg()
         tf.header.frame_id = "map"
         tf.child_frame_id = "odom"
-        tf.transform.rotation.x = 0.0
-        tf.transform.rotation.y = 0.0
-        tf.transform.rotation.z = 0.0
-        tf.transform.rotation.w = 1.0
         tf.transform.translation.x = 0.0
         tf.transform.translation.y = 0.0
         tf.transform.translation.z = 0.0
         self.tf_publisher.sendTransform(tf)
-        self.static_tf_publisher.sendTransform(tf)
 
         msg = OccupancyGrid()
-        msg.header.frame_id = 'map'
         msg.header.stamp = now.to_msg()
-        msg.info.map_load_time = now.to_msg()
+        msg.header.frame_id = 'map'
         msg.info.resolution = 0.01
         msg.info.width = 100
         msg.info.height = 100
-        msg.info.origin.position.x = -50.0
-        msg.info.origin.position.y = -50.0
+        msg.info.origin.position.x = -(msg.info.width * msg.info.resolution / 2)
+        msg.info.origin.position.y = -(msg.info.height * msg.info.resolution / 2)
         msg.data = [0]*(msg.info.width * msg.info.height)
         self.map_publisher.publish(msg)
 

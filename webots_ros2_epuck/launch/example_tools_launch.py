c45f13628796d57e6a97aff0d48791b89617886c
Darko Lukic <lukicdarkoo@gmail.com>
2020-04-15 11:04:58 +0200

    Fix linter errors

--- webots_ros2_epuck/launch/example_tools_launch.py
+++ webots_ros2_epuck/launch/example_tools_launch.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Launch additional tools for e-puck controller to allow visualization, mapping and navigation"""
+"""Launch additional tools for e-puck controller to allow visualization, mapping and navigation."""
 
 import os
 import launch

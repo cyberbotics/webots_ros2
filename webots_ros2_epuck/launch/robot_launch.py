403f42824ceb110533dd496f726ebcafd0aa4a06
Darko Lukic <lukicdarkoo@gmail.com>
2020-04-27 13:14:23 +0200

    Fix imports

--- webots_ros2_epuck/launch/robot_launch.py
+++ webots_ros2_epuck/launch/robot_launch.py
@@ -19,8 +19,9 @@
 import os
 import launch
 from launch import LaunchDescription
+from launch.actions import RegisterEventHandler, EmitEvent
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node, RegisterEventHandler, EmitEvent
+from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 from webots_ros2_core.utils import ControllerLauncher
 

40332d47bbd4e3ee293b6323a95f8394ed317365
Darko Lukic <lukicdarkoo@gmail.com>
2020-06-15 12:07:10 +0200

    Add variance

--- webots_ros2_core/webots_ros2_core/devices/light_sensor_device.py
+++ webots_ros2_core/webots_ros2_core/devices/light_sensor_device.py
@@ -72,6 +72,25 @@ class LightSensorDevice(Device):
                 qos_profile_sensor_data
             )
 
+    def __get_variance(self, raw_value):
+        table = self._wb_device.getLookupTable()
+
+        # Find relative standard deviation in lookup table
+        relative_std = None
+        for i in range(0, len(table) - 3, 3):
+            if table[i+1] < raw_value < table[i+3+1] or table[i+1] > raw_value > table[i+3+1]:
+                relative_std = table[i+2]
+                break
+        if relative_std is None:
+            if raw_value < table[1]:
+                relative_std = table[2]
+            else:
+                relative_std = table[-1]
+
+        # Calculate variance from the relative standard deviation
+        std = interpolate_lookup_table(raw_value, self._wb_device.getLookupTable()) * IRRADIANCE_TO_ILLUMINANCE * relative_std
+        return std**2
+
     def step(self):
         if self._disable:
             return
@@ -82,13 +101,15 @@ class LightSensorDevice(Device):
 
         stamp = Time(seconds=self._node.robot.getTime()).to_msg()
 
-        # Publish camera data
+        # Publish light sensor data
         if self._publisher.get_subscription_count() > 0 or self._always_publish:
             self._wb_device.enable(self._timestep)
             msg = Illuminance()
             msg.header.stamp = stamp
+            msg.header.frame_id = self._wb_device.getName()
             msg.illuminance = interpolate_lookup_table(self._wb_device.getValue(
             ), self._wb_device.getLookupTable()) * IRRADIANCE_TO_ILLUMINANCE
+            msg.variance = self.__get_variance(self._wb_device.getValue())
             self._publisher.publish(msg)
         else:
             self._wb_device.disable()

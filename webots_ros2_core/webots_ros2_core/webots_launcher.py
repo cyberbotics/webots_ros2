98c8addc02164b8a0b64a490a3cf982c50c4f64f
Darko Lukic <lukicdarkoo@gmail.com>
2020-06-17 10:35:25 +0200

    Make WebotsLauncher support substitutions

--- webots_ros2_core/webots_ros2_core/webots_launcher.py
+++ webots_ros2_core/webots_ros2_core/webots_launcher.py
@@ -16,37 +16,46 @@
 
 """This launcher simply start Webots."""
 
-import argparse
 import os
-import subprocess
 import sys
-
+from launch.actions import ExecuteProcess
+from launch.substitutions import LaunchConfiguration
+from launch import LaunchContext
 from webots_ros2_core.utils import get_webots_home
 
 
-def main(args=None):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--world', dest='world', default='', help='Path to the world to load.')
-    parser.add_argument('--mode', dest='mode', default='realtime', help='Startup mode.')
-    parser.add_argument('--no-gui', dest='noGui', action='store_true',
-                        help='Start Webots with minimal GUI.')
-    args, unknown = parser.parse_known_args()
-    webotsPath = get_webots_home()
-    if sys.platform == 'win32':
-        webotsPath = os.path.join(webotsPath, 'msys64', 'mingw64', 'bin')
-    command = [os.path.join(webotsPath, 'webots'), '--mode=' + args.mode, args.world]
-    if 'WEBOTS_ARGUMENTS' in os.environ:
-        for argument in os.environ['WEBOTS_ARGUMENTS'].split():
-            command.append(argument)
-    if args.noGui:
-        command.append('--stdout')
-        command.append('--stderr')
-        command.append('--batch')
-        command.append('--no-sandbox')
-        command.append('--minimize')
+class WebotsLauncher(ExecuteProcess):
+    def __init__(self, output='screen', **kwargs):
+        world = LaunchConfiguration('world')
+        no_gui = LaunchConfiguration('no_gui', default=False)
+        mode = LaunchConfiguration('mode', default='realtime')
 
-    subprocess.call(command)
+        context = LaunchContext()
 
+        # Add `webots` executable to command
+        webots_path = get_webots_home()
+        if sys.platform == 'win32':
+            webots_path = os.path.join(webots_path, 'msys64', 'mingw64', 'bin')
+        webots_cmd = [os.path.join(webots_path, 'webots')]
 
-if __name__ == '__main__':
-    main()
+        # Add `world`
+        webots_cmd += [world]
+
+        # Add parameters to hide GUI if needed
+        if no_gui.perform(context).lower() in ['true', '1']:
+            webots_cmd += [
+                '--stdout',
+                '--stderr',
+                '--batch',
+                '--no-sandbox',
+                '--minimize'
+            ]
+
+        # Add mode
+        webots_cmd += ['--mode=' + mode.perform(context)]
+
+        super().__init__(
+            cmd=webots_cmd,
+            output=output,
+            **kwargs
+        )

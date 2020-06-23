c09ce8eac7ac657972522c9dd39f8c22cf937a87
DavidMansolino <David.Mansolino@cyberbotics.com>
2020-06-23 16:20:54 +0200

    fix setup

--- webots_ros2_importer/setup.py
+++ webots_ros2_importer/setup.py
@@ -11,7 +11,7 @@ data_files.append(('share/' + package_name, ['package.xml']))
 setup(
     name=package_name,
     version='0.0.2',
-    packages=[package_name],
+    packages=[package_name, package_name + '.urdf2webots.urdf2webots'],
     data_files=data_files,
     install_requires=[
         'setuptools'

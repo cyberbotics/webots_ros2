6867db7ecd6c7da5c9af9ac80f493cf78dd64546
DavidMansolino <David.Mansolino@cyberbotics.com>
2020-08-06 09:34:28 +0200

    Use brew python path.

--- webots_ros2_core/webots_ros2_core/utils.py
+++ webots_ros2_core/webots_ros2_core/utils.py
@@ -94,6 +94,11 @@ def append_webots_python_lib_to_path():
     if get_webots_version_major_number() <= 2019:
         sys.path.append(os.path.join(os.environ['WEBOTS_HOME'], 'lib', 'python%d%d' %
                         (sys.version_info[0], sys.version_info[1])))
+    elif sys.platform == 'darwin':
+        sys.path.append(os.path.join(os.environ['WEBOTS_HOME'],
+                                     'lib',
+                                     'controller',
+                                     'python%d%d_brew' % (sys.version_info[0], sys.version_info[1])))
     else:
         sys.path.append(os.path.join(os.environ['WEBOTS_HOME'],
                                      'lib',

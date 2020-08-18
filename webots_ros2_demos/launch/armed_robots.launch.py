6e1136e7a5f2684d7639bc61547486797fe5a1d4
DavidMansolino <David.Mansolino@cyberbotics.com>
2020-08-18 16:01:03 +0200

    Fix armed demo.

--- webots_ros2_demos/launch/armed_robots.launch.py
+++ webots_ros2_demos/launch/armed_robots.launch.py
@@ -40,8 +40,8 @@ def generate_launch_description():
                                        node_namespace='abb',
                                        parameters=[{'synchronization': synchronization}],
                                        output='screen')
-    Ure5controller = ControllerLauncher(package='webots_ros2_universal_robot',
-                                        node_executable='universal_robot',
+    Ure5controller = ControllerLauncher(package='webots_ros2_core',
+                                        node_executable='webots_robotic_arm_node',
                                         # this argument should match the 'name' field
                                         # of the robot in Webots
                                         arguments=['--webots-robot-name=UR5e'],

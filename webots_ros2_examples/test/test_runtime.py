bde3b08d8bcdb5ea3600961ed0c385fff18c5662
David Mansolino <mansolino.david@gmail.com>
2020-08-26 09:08:00 +0200

    Update test_runtime.py

--- webots_ros2_examples/test/test_runtime.py
+++ webots_ros2_examples/test/test_runtime.py
@@ -144,8 +144,8 @@ class TestController(unittest.TestCase):
         print(subprocess.check_output(['ls', '../../..']))
         print(subprocess.check_output(['ls', '../../../log']))
         print(subprocess.check_output(['ls', '../../../log/latest_test']))
-        print(subprocess.check_output(['ls', '../../../log/latest_test/*']))
-        print(subprocess.check_output(['cat', '../../../log/latest_test/*/*.log']))
+        print(subprocess.check_output(['ls', '../../../log/latest_test/webots_ros2_examples']))
+        print(subprocess.check_output(['cat', '../../../log/latest_test/webots_ros2_examples/*.log']))
         print('---------------')
         condition = check_topic_condition(
             self.node,

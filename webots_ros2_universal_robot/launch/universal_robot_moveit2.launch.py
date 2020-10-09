7afe185967ada3c6b1d7e649145a4d4defa1572a
Darko Lukic <lukicdarkoo@gmail.com>
2020-10-09 17:10:48 +0200

    Add MoveIt2 configuration files

---
+++ webots_ros2_universal_robot/launch/universal_robot_moveit2.launch.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+
+# Copyright 1996-2019 Cyberbotics Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Launch MoveIt2 in RViz2."""
+
+# Reference: https://github.com/ros-planning/moveit2/blob/main/moveit_demo_nodes/run_move_group/launch/run_move_group.launch.py
+
+import os
+import yaml
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+
+def load_file(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+
+    try:
+        with open(absolute_file_path, 'r') as file:
+            return file.read()
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
+        return None
+
+
+def load_yaml(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+
+    try:
+        with open(absolute_file_path, 'r') as file:
+            return yaml.safe_load(file)
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
+        return None
+
+
+def generate_launch_description():
+
+    # planning_context
+    robot_description_config = None
+    with open('/tmp/webots_robot_UR5e.urdf', 'r') as file:
+        robot_description_config = file.read()
+    robot_description = {'robot_description': robot_description_config}
+
+    robot_description_semantic_config = load_file(
+        'webots_ros2_universal_robot', 'config/ur5e.srdf')
+    robot_description_semantic = {
+        'robot_description_semantic': robot_description_semantic_config}
+
+    kinematics_yaml = load_yaml(
+        'webots_ros2_universal_robot', 'config/kinematics.yaml')
+    robot_description_kinematics = {
+        'robot_description_kinematics': kinematics_yaml}
+
+    # Planning Functionality
+    ompl_planning_pipeline_config = {'move_group': {
+        'planning_plugin': 'ompl_interface/OMPLPlanner',
+        'request_adapters': """default_planner_request_adapters/AddTimeOptimalParameterization default_planner_request_adapters/FixWorkspaceBounds default_planner_request_adapters/FixStartStateBounds default_planner_request_adapters/FixStartStateCollision default_planner_request_adapters/FixStartStatePathConstraints""",
+        'start_state_max_bounds_error': 0.1}}
+    ompl_planning_yaml = load_yaml(
+        'webots_ros2_universal_robot', 'config/ompl_planning.yaml')
+    ompl_planning_pipeline_config['move_group'].update(ompl_planning_yaml)
+
+    # Trajectory Execution Functionality
+    controllers_yaml = load_yaml('run_move_group', 'config/controllers.yaml')
+    moveit_controllers = {'moveit_simple_controller_manager': controllers_yaml,
+                          'moveit_controller_manager': 'moveit_simple_controller_manager/MoveItSimpleControllerManager'}
+
+    trajectory_execution = {'moveit_manage_controllers': True,
+                            'trajectory_execution.allowed_execution_duration_scaling': 1.2,
+                            'trajectory_execution.allowed_goal_duration_margin': 0.5,
+                            'trajectory_execution.allowed_start_tolerance': 0.01}
+
+    planning_scene_monitor_parameters = {"publish_planning_scene": True,
+                                         "publish_geometry_updates": True,
+                                         "publish_state_updates": True,
+                                         "publish_transforms_updates": True}
+
+    # Start the actual move_group node/action server
+    run_move_group_node = Node(package='moveit_ros_move_group',
+                               executable='move_group',
+                               output='screen',
+                               parameters=[robot_description,
+                                           robot_description_semantic,
+                                           kinematics_yaml,
+                                           ompl_planning_pipeline_config,
+                                           trajectory_execution,
+                                           moveit_controllers,
+                                           planning_scene_monitor_parameters])
+
+    # RViz
+    rviz_config_file = get_package_share_directory(
+        'run_move_group') + "/launch/run_move_group.rviz"
+    rviz_node = Node(package='rviz2',
+                     executable='rviz2',
+                     name='rviz2',
+                     output='log',
+                     arguments=['-d', rviz_config_file],
+                     parameters=[robot_description,
+                                 robot_description_semantic,
+                                 ompl_planning_pipeline_config,
+                                 kinematics_yaml])
+
+    # Static TF
+    static_tf = Node(package='tf2_ros',
+                     executable='static_transform_publisher',
+                     name='static_transform_publisher',
+                     output='log',
+                     arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'world', 'base_link'])
+
+    # Publish TF
+    robot_state_publisher = Node(package='robot_state_publisher',
+                                 executable='robot_state_publisher',
+                                 name='robot_state_publisher',
+                                 output='both',
+                                 parameters=[robot_description])
+
+    return LaunchDescription([rviz_node, static_tf, robot_state_publisher, run_move_group_node])

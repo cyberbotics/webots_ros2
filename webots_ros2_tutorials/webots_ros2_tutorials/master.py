e75a9b0239a3e180a1128cb219a31cf3b767aa86
harsh <harsh.kakashniya@gmail.com>
2020-11-01 23:44:49 -0600

    Chore : changed proto name

--- webots_ros2_tutorials/webots_ros2_tutorials/master.py
+++ webots_ros2_tutorials/webots_ros2_tutorials/master.py
@@ -22,9 +22,9 @@ class LineFollower(Node):
     def __init__(self):
         super().__init__('linefollower_cmdvel')
         # Subscribe IR sensors
-        self.subs_right_ir = self.create_subscription(Float64, 'right_IR', self.rightIR_cb, 1)
-        self.subs_left_ir = self.create_subscription(Float64, 'left_IR', self.leftIR_cb, 1)
-        self.subs_mid_ir = self.create_subscription(Float64, 'mid_IR', self.midIR_cb, 1)
+        self.subs_right_ir = self.create_subscription(Float64, 'right_IR', self.right_infrared_callback, 1)
+        self.subs_left_ir = self.create_subscription(Float64, 'left_IR', self.left_infrared_callback, 1)
+        self.subs_mid_ir = self.create_subscription(Float64, 'mid_IR', self.mid_infrared_callback, 1)
         # Publish cmd vel
         self.pubs_cmdvel = self.create_publisher(Twist, 'cmd_vel', 1)
 
@@ -66,14 +66,14 @@ class LineFollower(Node):
         self.stop = False
 
     # Call backs to update sensor reading variables
-    def rightIR_cb(self, msg):
+    def right_infrared_callback(self, msg):
         self.GS_RIGHT = msg.data
         self.LineFollowingModule()
 
-    def leftIR_cb(self, msg):
+    def left_infrared_callback(self, msg):
         self.GS_LEFT = msg.data
 
-    def midIR_cb(self, msg):
+    def mid_infrared_callback(self, msg):
         self.GS_MID = msg.data
 
 

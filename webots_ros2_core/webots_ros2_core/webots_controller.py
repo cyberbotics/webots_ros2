8a1307a04176882b7c728f0822e6fb89fd47a6e7
Darko Lukic <lukicdarkoo@gmail.com>
2021-01-04 16:01:05 +0100

    Fix

--- webots_ros2_core/webots_ros2_core/webots_controller.py
+++ webots_ros2_core/webots_ros2_core/webots_controller.py
@@ -1,3 +1,19 @@
+#!/usr/bin/env python
+
+# Copyright 1996-2020 Cyberbotics Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import sys
 from webots_ros2_core.utils import append_webots_python_lib_to_path
 
@@ -7,5 +23,6 @@ except Exception as e:
     sys.stderr.write('"WEBOTS_HOME" is not correctly set.')
     raise e
 
-#  deepcode ignore C0413: <comment the reason here>
+#  deepcode ignore C0413: This module is just a proxy to `controller`
+#  deepcode ignore W0401: This module is just a proxy to `controller`
 from controller import *

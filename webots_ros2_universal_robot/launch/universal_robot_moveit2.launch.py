0149799d51f30a0ae0dd0132dd4480fdc7defe4d
Darko Lukic <lukicdarkoo@gmail.com>
2021-01-06 18:28:02 +0100

    Snapshot

--- webots_ros2_universal_robot/launch/universal_robot_moveit2.launch.py
+++ webots_ros2_universal_robot/launch/universal_robot_moveit2.launch.py
@@ -22,6 +22,8 @@ import os
 import yaml
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 
 
@@ -48,84 +50,57 @@ def load_yaml(package_name, file_path):
 
 
 def generate_launch_description():
+    package_dir = get_package_share_directory('webots_ros2_universal_robot')
 
-    # planning_context
+    # Webots
+    webots = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('webots_ros2_core'), 'launch', 'robot_launch.py')
+        ),
+        launch_arguments={
+            'executable': 'webots_robotic_arm_node',
+            'world': os.path.join(package_dir, 'worlds', 'universal_robot_rviz.wbt')
+        }.items()
+    )
+
+    # MoveIt Config
     robot_description_config = None
-    with open('/tmp/webots_robot_UR5e.urdf', 'r') as file:
+    with open('/tmp/webots_robot_base_link.urdf', 'r') as file:
         robot_description_config = file.read()
     robot_description = {'robot_description': robot_description_config}
-
-    robot_description_semantic_config = load_file(
-        'webots_ros2_universal_robot', 'config/ur5e.srdf')
     robot_description_semantic = {
-        'robot_description_semantic': robot_description_semantic_config}
-
-    kinematics_yaml = load_yaml(
-        'webots_ros2_universal_robot', 'config/kinematics.yaml')
+        'robot_description_semantic': load_file('webots_ros2_universal_robot', 'config/ur5e.srdf')
+    }
     robot_description_kinematics = {
-        'robot_description_kinematics': kinematics_yaml}
-
-    # Planning Functionality
-    ompl_planning_pipeline_config = {'move_group': {
-        'planning_plugin': 'ompl_interface/OMPLPlanner',
-        'request_adapters': """default_planner_request_adapters/AddTimeOptimalParameterization default_planner_request_adapters/FixWorkspaceBounds default_planner_request_adapters/FixStartStateBounds default_planner_request_adapters/FixStartStateCollision default_planner_request_adapters/FixStartStatePathConstraints""",
-        'start_state_max_bounds_error': 0.1}}
-    ompl_planning_yaml = load_yaml(
-        'webots_ros2_universal_robot', 'config/ompl_planning.yaml')
-    ompl_planning_pipeline_config['move_group'].update(ompl_planning_yaml)
-
-    # Trajectory Execution Functionality
-    controllers_yaml = load_yaml('run_move_group', 'config/controllers.yaml')
-    moveit_controllers = {'moveit_simple_controller_manager': controllers_yaml,
-                          'moveit_controller_manager': 'moveit_simple_controller_manager/MoveItSimpleControllerManager'}
-
-    trajectory_execution = {'moveit_manage_controllers': True,
-                            'trajectory_execution.allowed_execution_duration_scaling': 1.2,
-                            'trajectory_execution.allowed_goal_duration_margin': 0.5,
-                            'trajectory_execution.allowed_start_tolerance': 0.01}
-
-    planning_scene_monitor_parameters = {"publish_planning_scene": True,
-                                         "publish_geometry_updates": True,
-                                         "publish_state_updates": True,
-                                         "publish_transforms_updates": True}
-
-    # Start the actual move_group node/action server
-    run_move_group_node = Node(package='moveit_ros_move_group',
-                               executable='move_group',
-                               output='screen',
-                               parameters=[robot_description,
-                                           robot_description_semantic,
-                                           kinematics_yaml,
-                                           ompl_planning_pipeline_config,
-                                           trajectory_execution,
-                                           moveit_controllers,
-                                           planning_scene_monitor_parameters])
+        'robot_description_kinematics': load_yaml('webots_ros2_universal_robot', 'config/kinematics.yaml')
+    }
+    moveit_controllers = {
+        'moveit_simple_controller_manager': load_yaml('webots_ros2_universal_robot', 'config/controllers.yaml'),
+        'moveit_controller_manager': 'moveit_simple_controller_manager/MoveItSimpleControllerManager'
+    }
+    run_move_group_node = Node(
+        package='moveit_ros_move_group',
+        executable='move_group',
+        output='screen',
+        parameters=[
+            robot_description_semantic,
+            robot_description,
+            moveit_controllers,
+            robot_description_kinematics
+        ]
+    )
 
     # RViz
-    rviz_config_file = get_package_share_directory(
-        'run_move_group') + "/launch/run_move_group.rviz"
-    rviz_node = Node(package='rviz2',
-                     executable='rviz2',
-                     name='rviz2',
-                     output='log',
-                     arguments=['-d', rviz_config_file],
-                     parameters=[robot_description,
-                                 robot_description_semantic,
-                                 ompl_planning_pipeline_config,
-                                 kinematics_yaml])
+    rviz_config_file = os.path.join(package_dir, 'launch', 'universal_robot_moveit2.rviz')
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_config_file],
+        parameters=[
+            robot_description,
+            robot_description_semantic
+        ]
+    )
 
-    # Static TF
-    static_tf = Node(package='tf2_ros',
-                     executable='static_transform_publisher',
-                     name='static_transform_publisher',
-                     output='log',
-                     arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'world', 'base_link'])
-
-    # Publish TF
-    robot_state_publisher = Node(package='robot_state_publisher',
-                                 executable='robot_state_publisher',
-                                 name='robot_state_publisher',
-                                 output='both',
-                                 parameters=[robot_description])
-
-    return LaunchDescription([rviz_node, static_tf, robot_state_publisher, run_move_group_node])
+    return LaunchDescription([webots, rviz_node, run_move_group_node])
--- webots_ros2_universal_robot/setup.py
+++ webots_ros2_universal_robot/setup.py
@@ -21,15 +21,13 @@ launchers = [
     'launch/universal_robot_multiple.launch.py',
     'launch/universal_robot_rviz.launch.py',
     'launch/universal_robot_rviz_dynamic.launch.py',
-    'launch/universal_robot_moveit2.launch.py'
+    'launch/universal_robot_moveit2.launch.py',
+    'launch/universal_robot_moveit2.rviz'
 ]
 config = [
     'config/controllers.yaml',
-    'config/fake_controllers.yaml',
-    'config/joint_limits.yaml',
-    'config/kinematics.yaml',
-    'config/ompl_planning.yaml',
-    'config/ur5e.srdf'
+    'config/ur5e.srdf',
+    'config/kinematics.yaml'
 ]
 textures = []
 for rootPath, dirNames, fileNames in os.walk('worlds/textures'):
@@ -39,7 +37,7 @@ for rootPath, dirNames, fileNames in os.walk('worlds/textures'):
 
 data_files = []
 data_files.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
-data_files.append(('share/' + package_name, launchers))
+data_files.append(('share/' + package_name + '/launch', launchers))
 data_files.append(('share/' + package_name + '/config', config))
 data_files.append(('share/' + package_name + '/worlds', worlds))
 data_files.append(('share/' + package_name + '/worlds/textures', textures))

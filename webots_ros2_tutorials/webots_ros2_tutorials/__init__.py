6aa52d6a8ea01c8c7a7473456d88df912da27e56
Darko Lukic <lukicdarkoo@gmail.com>
2021-01-06 12:41:05 +0100

    Update copyright year

--- webots_ros2_tutorials/webots_ros2_tutorials/__init__.py
+++ webots_ros2_tutorials/webots_ros2_tutorials/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 1996-2020 Soft_illusion.
+# Copyright 1996-2021 Soft_illusion.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.

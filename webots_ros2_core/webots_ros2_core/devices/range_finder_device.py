a2c8a803fd408cb7d2e2d23f2a1f2667cb9f298d
Darko Lukic <lukicdarkoo@gmail.com>
2021-02-02 10:05:36 +0100

    CI fix

--- webots_ros2_core/webots_ros2_core/devices/range_finder_device.py
+++ webots_ros2_core/webots_ros2_core/devices/range_finder_device.py
@@ -16,7 +16,7 @@
 
 import numpy as np
 from sensor_msgs.msg import Image
-from rclpy.qos import DurabilityPolicy, HistoryPolicy, QoSProfile, qos_profile_sensor_data
+from rclpy.qos import qos_profile_sensor_data
 from .sensor_device import SensorDevice
 
 
@@ -72,20 +72,20 @@ class RangeFinderDevice(SensorDevice):
             msg.width = self._wb_device.getWidth()
             msg.is_bigendian = False
             msg.step = self._wb_device.getWidth() * 4
-            
-            range_image = self._wb_device.getRangeImage()
-            if range_image is None:
-              return
-            else:
-              image_array = np.array(range_image, dtype="float32")
-              # We pass `data` directly to we avoid using `data` setter.
-              # Otherwise ROS2 converts data to `array.array` which slows down the simulation as it copies memory internally.
-              # Both, `bytearray` and `array.array`, implement Python buffer protocol, so we should not see unpredictable
-              # behavior.
-              # deepcode ignore W0212: Avoid conversion from `bytearray` to `array.array`.msg._data = image_array.tobytes()
-              msg._data = image_array.tobytes()
-              msg.encoding = '32FC1'
 
-              self._image_publisher.publish(msg)
+            if self._wb_device.getRangeImage() is None:
+                return
+            image_array = np.array(self._wb_device.getRangeImage(), dtype="float32")
+
+            # We pass `data` directly to we avoid using `data` setter.
+            # Otherwise ROS2 converts data to `array.array` which slows down the simulation as it copies memory internally.
+            # Both, `bytearray` and `array.array`, implement Python buffer protocol, so we should not see unpredictable
+            # behavior.
+            # deepcode ignore W0212: Avoid conversion from `bytearray` to `array.array`.
+            msg._data = image_array.tobytes()
+            msg.encoding = '32FC1'
+
+            self._image_publisher.publish(msg)
+
         else:
             self._wb_device.disable()

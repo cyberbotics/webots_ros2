555b948dfd94343903522c33cd41722475704b25
Darko Lukic <lukicdarkoo@gmail.com>
2021-04-28 17:30:14 +0200

    Fix distane sensor plugin compilation

--- webots_ros2_cpp/src/plugins/distance_sensor/DistanceSensorPlugin.cpp
+++ webots_ros2_cpp/src/plugins/distance_sensor/DistanceSensorPlugin.cpp
@@ -1,19 +1,21 @@
 #include <webots_ros2_cpp/PluginInterface.hpp>
+#include <webots_ros2_cpp/WebotsNode.hpp>
 #include <webots/Supervisor.hpp>
 
 #include <string>
 #include <map>
 #include <iostream>
+#include <memory>
 
 namespace webots_ros2
 {
     class DistanceSensorPlugin : public PluginInterface
     {
     public:
-        DistanceSensor(const webots::WebotsNode *node, const std::map<std::string, std::string> &parameters)
+        DistanceSensorPlugin(std::shared_ptr<webots_ros2::WebotsNode> node, const std::map<std::string, std::string> &parameters)
         {
             mNode = node;
-            mName = parameters["name"];
+            mName = parameters.at("name");
         };
         virtual void step(int size) override
         {
@@ -22,11 +24,11 @@ namespace webots_ros2
 
     private:
         std::string mName;
-        std::shared_ptr<webots::Supervisor> mNode;
+        std::shared_ptr<webots_ros2::WebotsNode> mNode;
     };
 }
 
-extern "C" webots_ros2::PluginInterface* create_plugin()
+extern "C" webots_ros2::PluginInterface* create_plugin(std::shared_ptr<webots_ros2::WebotsNode> node, const std::map<std::string, std::string> &parameters)
 {
-    return new DistanceSensorPlugin();
+    return new webots_ros2::DistanceSensorPlugin(node, parameters);
 }

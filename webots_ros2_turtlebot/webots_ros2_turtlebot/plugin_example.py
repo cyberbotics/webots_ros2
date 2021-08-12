fe592afe8ed1e7a8b7dfb550afd2c0665ef56a74
Darko Lukic <lukicdarkoo@gmail.com>
2021-08-12 17:55:01 +0200

    Cleaner

--- webots_ros2_turtlebot/webots_ros2_turtlebot/plugin_example.py
+++ webots_ros2_turtlebot/webots_ros2_turtlebot/plugin_example.py
@@ -4,11 +4,11 @@ from webots_ros2_driver.controller import Node
 class PluginExample:
     def init(self, webots_node, properties):
         print('The init() method is called')
-        print('Properties:', properties)
+        print('  - properties:', properties)
 
-        print('Basic timestep:', int(webots_node.robot.getBasicTimeStep()))
-        print('Robot name:', webots_node.robot.getName())
-        print('Is robot?', webots_node.robot.getType() == Node.ROBOT)
+        print('  - basic timestep:', int(webots_node.robot.getBasicTimeStep()))
+        print('  - robot name:', webots_node.robot.getName())
+        print('  - is robot?', webots_node.robot.getType() == Node.ROBOT)
         print()
 
     def step(self):

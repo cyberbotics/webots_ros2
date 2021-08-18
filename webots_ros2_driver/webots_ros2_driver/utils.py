1583a0a44e4d5a431deb9158435e337f12ca556d
Darko Lukić <lukicdarkoo@gmail.com>
2021-08-18 16:47:40 +0200

    Update webots_ros2_driver/webots_ros2_driver/utils.py
    Co-authored-by: Stefania Pedrazzi <stefaniapedrazzi@users.noreply.github.com>

--- webots_ros2_driver/webots_ros2_driver/utils.py
+++ webots_ros2_driver/webots_ros2_driver/utils.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""This launcher simply start Webots."""
+"""This launcher simply starts Webots."""
 
 import os
 import re

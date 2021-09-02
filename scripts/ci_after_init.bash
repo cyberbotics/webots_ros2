4aca0b406cbbff65f06214f27ba1dfe463fd6893
Benjamin Hug <benjamin.hug1997@gmail.com>
2021-09-02 08:28:49 +0200

    ci_after_init install foxy turtlebot3

--- scripts/ci_after_init.bash
+++ scripts/ci_after_init.bash
@@ -4,3 +4,7 @@ apt update
 apt install -y wget dialog apt-utils psmisc
 wget https://github.com/cyberbotics/webots/releases/download/R${WEBOTS_VERSION}/webots_${WEBOTS_VERSION}_amd64.deb -O /tmp/webots.deb
 apt install -y /tmp/webots.deb xvfb
+
+if ${ROS_DISTRO} = "foxy" then
+    apt install ros-foxy-turtlebot3-cartographer
+    apt install ros-foxy-turtlebot3-navigation2

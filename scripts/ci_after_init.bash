42b8e53ef8feb5c46aa52145fb7945f2ac1a376e
Darko Lukić <lukicdarkoo@gmail.com>
2021-09-02 09:42:35 +0200

    Update ci_after_init.bash

--- scripts/ci_after_init.bash
+++ scripts/ci_after_init.bash
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+ROS_DISTRO=$1
+
 apt update
 apt install -y wget dialog apt-utils psmisc
 wget https://github.com/cyberbotics/webots/releases/download/R${WEBOTS_VERSION}/webots_${WEBOTS_VERSION}_amd64.deb -O /tmp/webots.deb
 apt install -y /tmp/webots.deb xvfb
+
+if [ "${ROS_DISTRO}" = "foxy" ]; then
+    apt install ros-foxy-turtlebot3-cartographer
+    apt install ros-foxy-turtlebot3-navigation2
+fi

b3175374d1a4e7fda27f8c5b3f4b387e67dccb25
BenjaminHug8 <61198898+BenjaminHug8@users.noreply.github.com>
2021-09-03 09:38:48 +0200

    Update scripts/ci_after_init.bash
    Co-authored-by: Darko Lukić <lukicdarkoo@gmail.com>

--- scripts/ci_after_init.bash
+++ scripts/ci_after_init.bash
@@ -8,7 +8,7 @@ apt install -y wget dialog apt-utils psmisc
 wget https://github.com/cyberbotics/webots/releases/download/R${WEBOTS_VERSION}/webots_${WEBOTS_VERSION}_amd64.deb -O /tmp/webots.deb
 apt install -y /tmp/webots.deb xvfb
 
-# The following packages are only yupported by ROS2 Foxy, done here to be sure that ROS_DISTRO is available
+# The following packages are only available in the ROS 2 Foxy distribution. Therefore, we cannot include them in the package.xml, but we have to install them manually here.
 if [ "${ROS_DISTRO}" = "foxy" ]; then
     apt install -y ros-foxy-turtlebot3-cartographer ros-foxy-turtlebot3-navigation2
 fi

62ec241c7777cdb5b848ef1c65f6d542ab055241
Benjamin Hug <benjamin.hug1997@gmail.com>
2021-09-03 08:43:26 +0200

    Comment after_init

--- scripts/ci_after_init.bash
+++ scripts/ci_after_init.bash
@@ -8,6 +8,7 @@ apt install -y wget dialog apt-utils psmisc
 wget https://github.com/cyberbotics/webots/releases/download/R${WEBOTS_VERSION}/webots_${WEBOTS_VERSION}_amd64.deb -O /tmp/webots.deb
 apt install -y /tmp/webots.deb xvfb
 
+# The following packages are only yupported by ROS2 Foxy, done here to be sure that ROS_DISTRO is available
 if [ "${ROS_DISTRO}" = "foxy" ]; then
     apt install -y ros-foxy-turtlebot3-cartographer ros-foxy-turtlebot3-navigation2
 fi

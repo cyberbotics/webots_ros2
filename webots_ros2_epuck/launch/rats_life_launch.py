51538037a56a827530d58f25d4d0584c597bef61
Benjamin Hug <benjamin.hug1997@gmail.com>
2021-09-10 16:56:42 +0200

    fix CI rolling nav2bringup

--- webots_ros2_epuck/launch/rats_life_launch.py
+++ webots_ros2_epuck/launch/rats_life_launch.py
@@ -18,76 +18,85 @@
 
 import os
 from launch.substitutions import LaunchConfiguration
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, LogInfo
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch import LaunchDescription
-from ament_index_python.packages import get_package_share_directory
+from ament_index_python.packages import get_package_share_directory, get_packages_with_prefixes
 from launch_ros.actions import Node
 from launch.actions import ExecuteProcess
 
 
 def generate_launch_description():
+    launch_description_nodes = []
     package_dir = get_package_share_directory('webots_ros2_epuck')
     use_sim_time = LaunchConfiguration('use_sim_time', default=True)
     synchronization = LaunchConfiguration('synchronization', default=True)
     world = LaunchConfiguration('world', default='rats_life_benchmark.wbt')
 
-    # Webots
-    webots_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(package_dir, 'launch', 'robot_launch.py')
-        ),
-        launch_arguments={
-            'synchronization': synchronization,
-            'use_sim_time': 'true',
-            'world': world
-        }.items()
+    # Webots node
+    launch_description_nodes.append(
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(package_dir, 'launch', 'robot_launch.py')
+            ),
+            launch_arguments={
+                'synchronization': synchronization,
+                'use_sim_time': 'true',
+                'world': world
+            }.items()
+        )
     )
 
-    nav2_map = os.path.join(package_dir, 'resource', 'map_rats_life.yaml')
+    # Check if nav2_bringup is installed
+    if 'nav2_bringup' in get_packages_with_prefixes():
+        # Rviz node
+        launch_description_nodes.append(
+            Node(
+                package='rviz2',
+                executable='rviz2',
+                name='rviz2',
+                arguments=['-d', os.path.join(get_package_share_directory('nav2_bringup'), 'rviz', 'nav2_default_view.rviz')],
+                parameters=[{'use_sim_time': use_sim_time}],
+                output='screen'
+            )
+        )
 
-    # Launch complete Navigation2 with `amcl` (particle filter to track the pose of a robot)
-    nav2 = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('nav2_bringup'), 'launch', 'bringup_launch.py')),
-        launch_arguments=[
-            ('map', nav2_map),
-            ('use_sim_time', use_sim_time),
-            ('params_file', os.path.join(package_dir, 'resource', 'nav2_params.yaml'))
-        ]
-    )
+        # Navigation
+        nav2_map = os.path.join(package_dir, 'resource', 'map_rats_life.yaml')
 
-    rviz = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        arguments=['-d', os.path.join(get_package_share_directory('nav2_bringup'), 'rviz', 'nav2_default_view.rviz')],
-        parameters=[{'use_sim_time': use_sim_time}],
-        output='screen'
-    )
-
-    # Set initial position of the robot within the provided map.
-    # The initial position can be also be set in RViz2 menu `2D Pose Estimate`.
-    initial_position = ExecuteProcess(
-        cmd=[
-            'ros2',
-            'topic',
-            'pub',
-            '--once',
-            '/initialpose',
-            'geometry_msgs/msg/PoseWithCovarianceStamped',
-            '{\
+        launch_description_nodes.append(
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    os.path.join(get_package_share_directory('nav2_bringup'), 'launch', 'bringup_launch.py')
+                ),
+                launch_arguments=[
+                    ('map', nav2_map),
+                    ('use_sim_time', use_sim_time),
+                    ('params_file', os.path.join(package_dir, 'resource', 'nav2_params.yaml'))
+                ],
+            )
+        )
+        # Set initial position of the robot within the provided map.
+        # The initial position can be also be set in RViz2 menu `2D Pose Estimate`.
+        launch_description_nodes.append(ExecuteProcess(
+            cmd=[
+                'ros2',
+                'topic',
+                'pub',
+                '--once',
+                '/initialpose',
+                'geometry_msgs/msg/PoseWithCovarianceStamped',
+                '{\
                 "header": { "frame_id": "map" },\
                 "pose": { "pose": {\
                     "position": { "x": 0.005, "y": 0.0, "z": 0.0 },\
                     "orientation": { "x": 0.0, "y": 0.0, "z": 0.0, "w": 1.0 }}\
                 }\
             }'
-        ]
-    )
+            ]
+        ))
+    else:
+        launch_description_nodes.append(LogInfo(msg='Navigation2 is not installed, navigation functionality is disabled'))
 
-    return LaunchDescription([
-        webots_launch,
-        nav2,
-        rviz,
-        initial_position,
-    ])
+    # Launch descriptor
+    return LaunchDescription(launch_description_nodes)

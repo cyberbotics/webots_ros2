9225e73e4831febacdef9538a395905ece060870
BenjaminHug8 <61198898+BenjaminHug8@users.noreply.github.com>
2021-09-13 16:10:37 +0200

    Utilize `webots_ros2_driver` in `webots_ros2_epuck` (#296)
    * initial commit progress on epuck alone scenario
    
    * 1st draft full conversion
    
    * system tests in dev
    
    * basic epuck driver converted
    
    * system tests draft
    
    * system tests on simple epuck ok
    
    * Apply suggestions from code review
    
    Co-authored-by: Darko Lukić <lukicdarkoo@gmail.com>
    
    * modif review PR
    
    * CI
    
    * fix CI
    
    * fix CI package test has tf2_ros
    
    * remove testFootPrint
    
    * fix CI driver plugin python test
    
    * System tests for scenarios
    
    * System tests scenarios CI foxy
    
    * Change test plan
    
    * more delay plan test
    
    * more delay plan test fix flake
    
    * fix ci rolling
    
    * fix CI rolling nav2bringup
    
    * fix CI rolling nav2bringup pckg required
    
    * removed test rats scenario
    
    * flake8
    
    * removed file test rats scenario
    
    * review changes
    
    * review changes + flake
    
    * revert changes du to Tesla PR
    
    * fix test tools
    
    * adapt topics name
    
    Co-authored-by: Darko Lukić <lukicdarkoo@gmail.com>

--- webots_ros2_epuck/launch/robot_launch.py
+++ webots_ros2_epuck/launch/robot_launch.py
@@ -17,28 +17,90 @@
 """Launch Webots e-puck driver."""
 
 import os
+import pathlib
+import launch
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions.path_join_substitution import PathJoinSubstitution
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
 from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
+from webots_ros2_driver.webots_launcher import WebotsLauncher
 
 
 def generate_launch_description():
     package_dir = get_package_share_directory('webots_ros2_epuck')
     world = LaunchConfiguration('world')
+    robot_description = pathlib.Path(os.path.join(package_dir, 'resource', 'epuck_webots.urdf')).read_text()
+    ros2_control_params = os.path.join(package_dir, 'resource', 'ros2_control.yml')
+    use_sim_time = LaunchConfiguration('use_sim_time', default=True)
 
-    webots = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('webots_ros2_core'), 'launch', 'robot_launch.py')
-        ),
-        launch_arguments=[
-            ('package', 'webots_ros2_epuck'),
-            ('executable', 'driver'),
-            ('world', PathJoinSubstitution([package_dir, 'worlds', world])),
+    webots = WebotsLauncher(
+        world=PathJoinSubstitution([package_dir, 'worlds', world])
+    )
+
+    controller_manager_timeout = ['--controller-manager-timeout', '50']
+    controller_manager_prefix = 'python.exe' if os.name == 'nt' else ''
+
+    diffdrive_controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner.py',
+        output='screen',
+        prefix=controller_manager_prefix,
+        arguments=['diffdrive_controller'] + controller_manager_timeout,
+        parameters=[
+            {'use_sim_time': use_sim_time},
+        ],
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package='controller_manager',
+        executable='spawner.py',
+        output='screen',
+        prefix=controller_manager_prefix,
+        arguments=['joint_state_broadcaster'] + controller_manager_timeout,
+        parameters=[
+            {'use_sim_time': use_sim_time},
+        ],
+    )
+
+    epuck_driver = Node(
+        package='webots_ros2_driver',
+        executable='driver',
+        output='screen',
+        parameters=[
+            {'robot_description': robot_description,
+             'use_sim_time': use_sim_time},
+            ros2_control_params
+        ],
+        remappings=[
+            ('/diffdrive_controller/cmd_vel_unstamped', '/cmd_vel'),
         ]
+    )
+
+    epuck_process = Node(
+        package='webots_ros2_epuck',
+        executable='epuck_node',
+        output='screen',
+        parameters=[
+            {'use_sim_time': use_sim_time},
+        ],
+    )
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{
+            'robot_description': '<robot name=""><link name=""/></robot>'
+        }],
+    )
+
+    footprint_publisher = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        output='screen',
+        arguments=['0', '0', '0', '0', '0', '0', 'base_link', 'base_footprint'],
     )
 
     return LaunchDescription([
@@ -47,5 +109,19 @@ def generate_launch_description():
             default_value='epuck_world.wbt',
             description='Choose one of the world files from `/webots_ros2_epuck/world` directory'
         ),
-        webots
+        joint_state_broadcaster_spawner,
+        diffdrive_controller_spawner,
+        webots,
+        robot_state_publisher,
+        epuck_driver,
+        footprint_publisher,
+        epuck_process,
+
+        # This action will kill all nodes once the Webots simulation has exited
+        launch.actions.RegisterEventHandler(
+            event_handler=launch.event_handlers.OnProcessExit(
+                target_action=webots,
+                on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
+            )
+        )
     ])

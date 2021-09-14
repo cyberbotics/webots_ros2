399fc3250f35d24b8bba328730f2152162e68ad2
Darko Lukić <lukicdarkoo@gmail.com>
2021-09-14 14:12:19 +0200

    Update test_system_universal_robot.py

--- webots_ros2_tests/test/test_system_universal_robot.py
+++ webots_ros2_tests/test/test_system_universal_robot.py
@@ -75,11 +75,11 @@ class TestUniversalRobot(TestWebots):
 
     def setUp(self):
         self.__node = rclpy.create_node('driver_tester')
-        self.wait_for_clock(self.__node, messages_to_receive=20)
+        self.wait_for_clock(self.__node, messages_to_receive=120)
 
     def testAbbCaughtCan(self):
         # The robot should catch the can in the simulation.
-        self.wait_for_messages(self.__node, Range, '/abb/abbirb4600/object_present_sensor', timeout=350,
+        self.wait_for_messages(self.__node, Range, '/abb/abbirb4600/object_present_sensor', timeout=360,
                                condition=lambda message: message.range < 0.07)
 
     def tearDown(self):

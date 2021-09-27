48030e9e57321a7ac155593cf88561e876c766ea
Darko Lukic <lukicdarkoo@gmail.com>
2021-09-27 16:48:16 +0200

    Add new tiago

---
+++ webots_ros2_tiago/README.md
@@ -0,0 +1,5 @@
+# TIAGo
+
+This package provides a ROS2 interface example for the simulated TIAGo robot in Webots.
+
+Documentation is available [here](https://github.com/cyberbotics/webots_ros2/wiki/Example-TIAGo).
---
+++ webots_ros2_tiago/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+package_name = 'webots_ros2_tiago'
+data_files = []
+data_files.append(
+    ('share/ament_index/resource_index/packages', ['resource/' + package_name]))
+data_files.append(('share/' + package_name + '/launch', ['launch/robot_launch.py']))
+data_files.append(('share/' + package_name + '/resource', [
+    'resource/tiago_webots.urdf',
+    'resource/ros2_control.yml',
+    'resource/default.rviz',
+    'resource/map.pgm',
+    'resource/map.yaml',
+]))
+data_files.append(('share/' + package_name, ['package.xml']))
+data_files.append(('share/' + package_name + '/worlds', ['worlds/default.wbt', 'worlds/.default.wbproj']))
+
+setup(
+    name=package_name,
+    version='1.1.2',
+    packages=[],
+    data_files=data_files,
+    install_requires=['setuptools', 'launch'],
+    zip_safe=True,
+    author='Cyberbotics',
+    author_email='support@cyberbotics.com',
+    maintainer='Cyberbotics',
+    maintainer_email='support@cyberbotics.com',
+    keywords=['ROS', 'Webots', 'Robot', 'Simulation', 'Examples', 'TIAGo'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='TIAGo robots ROS2 interface for Webots.',
+    license='Apache License, Version 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'launch.frontend.launch_extension': ['launch_ros = launch_ros']
+    }
+)

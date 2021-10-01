3a91326c3df2597a1a217d82ab1a60cdd7ce976b
Darko Lukić <lukicdarkoo@gmail.com>
2021-10-01 14:13:28 +0200

    Delete unnecessary packages (#301)
    * Delete unnecessary packages
    
    * Delete deps
    
    * msgs
    
    * Add new tiago
    
    * wip
    
    * Fix
    
    * fix
    
    * Fix
    
    * Add
    
    * Timeout
    
    * Lighter
    
    * fix
    
    * exit
    
    * revert

--- webots_ros2_driver/src/WebotsNode.cpp
+++ webots_ros2_driver/src/WebotsNode.cpp
@@ -216,6 +216,7 @@ namespace webots_ros2_driver
   {
     if (mRobot->step(mStep) == -1) {
       mTimer->cancel();
+      exit(0);
       return;
     }
     for (std::shared_ptr<PluginInterface> plugin : mPlugins)
--- webots_ros2_tiago/setup.py
+++ webots_ros2_tiago/setup.py
@@ -1,20 +1,19 @@
-"""webots_ros2 package setup file."""
-
 from setuptools import setup
 
 package_name = 'webots_ros2_tiago'
 data_files = []
 data_files.append(
     ('share/ament_index/resource_index/packages', ['resource/' + package_name]))
-data_files.append(('share/' + package_name, ['launch/tiago.launch.py']))
-data_files.append(('share/' + package_name + '/worlds', [
-    'worlds/ros_tiago.wbt', 'worlds/.ros_tiago.wbproj',
-    'worlds/tiago++_example.wbt', 'worlds/.tiago++_example.wbproj'
+data_files.append(('share/' + package_name + '/launch', ['launch/robot_launch.py']))
+data_files.append(('share/' + package_name + '/resource', [
+    'resource/tiago_webots.urdf',
+    'resource/ros2_control.yml',
+    'resource/default.rviz',
+    'resource/map.pgm',
+    'resource/map.yaml',
 ]))
-data_files.append(('share/' + package_name + '/resource',
-                   ['resource/odometry.rviz', 'resource/tiago.yaml']))
 data_files.append(('share/' + package_name, ['package.xml']))
-
+data_files.append(('share/' + package_name + '/worlds', ['worlds/default.wbt', 'worlds/.default.wbproj']))
 
 setup(
     name=package_name,

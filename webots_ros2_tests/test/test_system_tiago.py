9404965d3e726519843bbca461068478609c089d
Benjamin Hug <benjamin.hug1997@gmail.com>
2021-11-12 15:40:20 +0100

    modify tiago system test condition galactic

--- webots_ros2_tests/test/test_system_tiago.py
+++ webots_ros2_tests/test/test_system_tiago.py
@@ -35,8 +35,10 @@ from webots_ros2_tests.utils import TestWebots, initialize_webots_test
 @pytest.mark.rostest
 def generate_test_description():
     initialize_webots_test()
-    if ('ROS_DISTRO' in os.environ and os.environ['ROS_DISTRO'] == 'rolling'):
-        pytest.skip('Tests are not support for ROS Rolling')
+    # ROS_DISTRO is Rolling or Galactic, skip test as some required package are missing (cf. ci_after_init.bash)
+    if 'ROS_DISTRO' in os.environ and \
+            (os.environ['ROS_DISTRO'] == 'rolling' or os.environ['ROS_DISTRO'] == 'galactic'):
+        pytest.skip('ROS_DISTRO is rolling, skip this test')
 
     tiago_webots = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(

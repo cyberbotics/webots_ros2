04ed9e0656ee7501b64da3a63d19b672462c7ff2
Benjamin Hug <benjamin.hug1997@gmail.com>
2021-12-22 08:32:42 +0100

    restore webots version ci test

--- scripts/ci_after_init.bash
+++ scripts/ci_after_init.bash
@@ -5,7 +5,7 @@ ROS_REPO=$2
 
 apt update
 apt install -y wget dialog apt-utils psmisc
-wget https://github.com/cyberbotics/webots/releases/download/nightly_15_12_2021/webots_${WEBOTS_VERSION}_amd64.deb -O /tmp/webots.deb
+wget https://github.com/cyberbotics/webots/releases/download/R${WEBOTS_VERSION}/webots_${WEBOTS_VERSION}_amd64.deb -O /tmp/webots.deb
 apt install -y /tmp/webots.deb xvfb
 
 # The following packages are only available in the ROS 2 Foxy distribution. Therefore, we cannot include them in the package.xml, but we have to install them manually here.

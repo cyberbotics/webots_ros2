37c25731cae79f0eff37f9ee964fec5cbe13c16f
Benjamin Hug <benjamin.hug1997@gmail.com>
2022-01-17 16:04:01 +0100

    test testing

--- scripts/ci_before_init_embed.bash
+++ scripts/ci_before_init_embed.bash
@@ -5,4 +5,4 @@ export WEBOTS_OFFSCREEN=1
 export CI=1
 export DEBIAN_FRONTEND=noninteractive
 export QTWEBENGINE_DISABLE_SANDBOX=1
-export ROS_REPOSITORY_PATH="http://packages.ros.org/ros2/ubuntu"
+export ROS_REPOSITORY_PATH="http://packages.ros.org/ros2-testing/ubuntu"

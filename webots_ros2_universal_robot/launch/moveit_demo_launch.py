50836cc973c0b397c9babb4322564f47d850ccd1
Benjamin Hug <benjamin.hug1997@gmail.com>
2022-01-18 15:36:30 +0100

    loginfo launch file

--- webots_ros2_universal_robot/launch/moveit_demo_launch.py
+++ webots_ros2_universal_robot/launch/moveit_demo_launch.py
@@ -19,17 +19,18 @@
 import os
 import pathlib
 import yaml
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, LogInfo
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
+from ament_index_python.packages import get_package_share_directory, get_packages_with_prefixes
 
 
 PACKAGE_NAME = 'webots_ros2_universal_robot'
 
 
 def generate_launch_description():
+    launch_description_nodes = []
     package_dir = get_package_share_directory(PACKAGE_NAME)
 
     def load_file(filename):
@@ -38,49 +39,62 @@ def generate_launch_description():
     def load_yaml(filename):
         return yaml.safe_load(load_file(filename))
 
-    description = {'robot_description': load_file('moveit_ur5e_description.urdf')}
-    description_semantic = {'robot_description_semantic': load_file('moveit_ur5e.srdf')}
-    description_kinematics = {'robot_description_kinematics': load_yaml('moveit_kinematics.yaml')}
-    movegroup = {'move_group': load_yaml('moveit_movegroup.yaml')}
-    moveit_controllers = {
-        'moveit_controller_manager': 'moveit_simple_controller_manager/MoveItSimpleControllerManager',
-        'moveit_simple_controller_manager': load_yaml('moveit_controllers.yaml')
-    }
-    sim_time = {'use_sim_time': True}
-    run_move_group_node = Node(
-        package='moveit_ros_move_group',
-        executable='move_group',
-        output='screen',
-        parameters=[
-            description,
-            description_semantic,
-            description_kinematics,
-            moveit_controllers,
-            movegroup,
-            sim_time
-        ],
+    # Check if nav2_bringup is installed
+    if 'moveit' in get_packages_with_prefixes():
+        # Configuration
+        description = {'robot_description': load_file('moveit_ur5e_description.urdf')}
+        description_semantic = {'robot_description_semantic': load_file('moveit_ur5e.srdf')}
+        description_kinematics = {'robot_description_kinematics': load_yaml('moveit_kinematics.yaml')}
+        sim_time = {'use_sim_time': True}
+
+        # Rviz node
+        rviz_config_file = os.path.join(package_dir, 'resource', 'moveit_visualization.rviz')
+
+        launch_description_nodes.append(
+            Node(
+                package='rviz2',
+                executable='rviz2',
+                name='rviz2',
+                arguments=['-d', rviz_config_file],
+                parameters=[
+                    description,
+                    description_semantic,
+                    description_kinematics,
+                    sim_time
+                ],
+            )
+        )
+
+        # Navigation
+        movegroup = {'move_group': load_yaml('moveit_movegroup.yaml')}
+        moveit_controllers = {
+            'moveit_controller_manager': 'moveit_simple_controller_manager/MoveItSimpleControllerManager',
+            'moveit_simple_controller_manager': load_yaml('moveit_controllers.yaml')
+        }
+
+        launch_description_nodes.append(
+            Node(
+                package='moveit_ros_move_group',
+                executable='move_group',
+                output='screen',
+                parameters=[
+                    description,
+                    description_semantic,
+                    description_kinematics,
+                    moveit_controllers,
+                    movegroup,
+                    sim_time
+                ],
+            )
+        )
+    else:
+        launch_description_nodes.append(LogInfo(msg='"moveit" package is not installed, please install it in order to use this functionnality.'))
+
+    # Webots simulation with robot
+    launch_description_nodes.append(
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(package_dir, 'launch', 'robot_launch.py'))
+        )
     )
 
-    rviz_config_file = os.path.join(package_dir, 'resource', 'moveit_visualization.rviz')
-    rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        arguments=['-d', rviz_config_file],
-        parameters=[
-            description,
-            description_semantic,
-            description_kinematics,
-            sim_time
-        ],
-    )
-
-    robot = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(package_dir, 'launch', 'robot_launch.py'))
-    )
-
-    return LaunchDescription([
-        run_move_group_node,
-        rviz_node,
-        robot
-    ])
+    return LaunchDescription(launch_description_nodes)

008ba29e09b01edf50b5288395dcb37e955f69a9
Benjamin Hug <benjamin.hug1997@gmail.com>
2022-01-26 16:40:12 +0100

    test printenv

--- scripts/ci_after_init.bash
+++ scripts/ci_after_init.bash
@@ -17,6 +17,15 @@ apt update
 apt install -y wget dialog apt-utils psmisc
 wget https://github.com/cyberbotics/webots/releases/download/${WEBOTS_NIGHTLY_VERSION}/webots_${WEBOTS_RELEASE_VERSION}_amd64.deb -O /tmp/webots.deb
 apt install -y /tmp/webots.deb xvfb
+# Fix RViz in CI
+if [ "${ROS_DISTRO}" = "foxy" ]; then
+    apt install libxcb-xinerama0
+fi
+
+
+printenv
+
+exit 1;
 
 # The following packages are only available in the ROS 2 Foxy distribution. Therefore, we cannot include them in the package.xml, but we have to install them manually here.
 if [ "${ROS_DISTRO}" = "foxy" ]; then

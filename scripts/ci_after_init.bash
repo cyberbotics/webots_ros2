f07f4000b116c09b0011819af806b0eecac61f45
Benjamin Hug <benjamin.hug1997@gmail.com>
2022-01-26 12:53:01 +0100

    test without fix

--- scripts/ci_after_init.bash
+++ scripts/ci_after_init.bash
@@ -18,7 +18,6 @@ apt install -y wget dialog apt-utils psmisc
 wget https://github.com/cyberbotics/webots/releases/download/${WEBOTS_NIGHTLY_VERSION}/webots_${WEBOTS_RELEASE_VERSION}_amd64.deb -O /tmp/webots.deb
 apt install -y /tmp/webots.deb xvfb
 # Fix for RViz in CI
-apt install libxcb-xinerama0
 
 # The following packages are only available in the ROS 2 Foxy distribution. Therefore, we cannot include them in the package.xml, but we have to install them manually here.
 if [ "${ROS_DISTRO}" = "foxy" ]; then

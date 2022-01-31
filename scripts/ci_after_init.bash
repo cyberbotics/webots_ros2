c47e42be738324ea7bdff237661ee28606ce313d
Benjamin Hug <benjamin.hug1997@gmail.com>
2022-01-31 11:44:43 +0100

    multi tests printenv

--- scripts/ci_after_init.bash
+++ scripts/ci_after_init.bash
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+echo PRINTENV start
+printenv
+
 ROS_DISTRO=$1
 ROS_REPO=$2
 
@@ -17,10 +20,14 @@ apt update
 apt install -y wget dialog apt-utils psmisc
 wget https://github.com/cyberbotics/webots/releases/download/${WEBOTS_NIGHTLY_VERSION}/webots_${WEBOTS_RELEASE_VERSION}_amd64.deb -O /tmp/webots.deb
 apt install -y /tmp/webots.deb xvfb
-# Fix RViz in CI
-apt install libxcb-xinerama0
+
+echo PRINTENV webots
+printenv
 
 # The following packages are only available in the ROS 2 Foxy distribution. Therefore, we cannot include them in the package.xml, but we have to install them manually here.
 if [ "${ROS_DISTRO}" = "foxy" ]; then
     apt install -y ros-foxy-turtlebot3-cartographer ros-foxy-turtlebot3-navigation2
 fi
+
+echo PRINTENV turtle
+printenv

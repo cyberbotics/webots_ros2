a26c7e3f4cf8862cb7efaddbe9e55d332352a8d0
Benjamin Hug <benjamin.hug1997@gmail.com>
2022-01-31 13:54:56 +0100

    QT_PLUGIN_PATH

--- scripts/ci_after_init.bash
+++ scripts/ci_after_init.bash
@@ -25,4 +25,5 @@ fi
 
 ldd /usr/lib/x86_64-linux-gnu/qt5/plugins/platforms/libqxcb.so
 
+export QT_PLUGIN_PATH=/usr/lib/x86_64-linux-gnu/qt5/plugins
 export LD_LIBRARY_PATH=/usr/lib:$LD_LIBRARY_PATH

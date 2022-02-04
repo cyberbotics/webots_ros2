46ff0588c320f7df57eca5f61bf1fb9270e3c250
Benjamin Hug <61198898+BenjaminHug8@users.noreply.github.com>
2022-02-04 14:56:21 +0100

    Update Ros2RangeFinder.cpp

--- webots_ros2_driver/src/plugins/static/Ros2RangeFinder.cpp
+++ webots_ros2_driver/src/plugins/static/Ros2RangeFinder.cpp
@@ -27,14 +27,17 @@ namespace webots_ros2_driver
 
     assert(mRangeFinder != NULL);
 
+    const int width = mRangeFinder->getWidth();
+    const int height = mRangeFinder->getHeight();
+
     // Image publisher
     mImagePublisher = mNode->create_publisher<sensor_msgs::msg::Image>(mTopicName, rclcpp::SensorDataQoS().reliable());
     mImageMessage.header.frame_id = mFrameName;
-    mImageMessage.height = mRangeFinder->getHeight();
-    mImageMessage.width = mRangeFinder->getWidth();
+    mImageMessage.height = height;
+    mImageMessage.width = width;
     mImageMessage.is_bigendian = false;
-    mImageMessage.step = sizeof(unsigned char) * 4 * mRangeFinder->getWidth();
-    mImageMessage.data.resize(4 * mRangeFinder->getWidth() * mRangeFinder->getHeight());
+    mImageMessage.step = sizeof(unsigned char) * 4 * width;
+    mImageMessage.data.resize(4 * width * height);
     mImageMessage.encoding = sensor_msgs::image_encodings::TYPE_32FC1;
 
     // CameraInfo publisher
@@ -45,47 +48,45 @@ namespace webots_ros2_driver
     mCameraInfoPublisher = mNode->create_publisher<sensor_msgs::msg::CameraInfo>(mTopicName + "/camera_info", cameraInfoQos);
     mCameraInfoMessage.header.stamp = mNode->get_clock()->now();
     mCameraInfoMessage.header.frame_id = mFrameName;
-    mCameraInfoMessage.height = mRangeFinder->getHeight();
-    mCameraInfoMessage.width = mRangeFinder->getWidth();
+    mCameraInfoMessage.height = height;
+    mCameraInfoMessage.width = width;
     mCameraInfoMessage.distortion_model = "plumb_bob";
-    const double focalLength = 0.5 * mRangeFinder->getWidth() * (1 / tan(0.5 * mRangeFinder->getFov()));
+    const double focalLengthx = 0.5 * width * (1 / tan(0.5 * mRangeFinder->getFov()));
+    const double focalLengthy = 0.5 * height * (1 / tan(0.5 * mRangeFinder->getFov()));
     mCameraInfoMessage.d = {0.0, 0.0, 0.0, 0.0, 0.0};
     mCameraInfoMessage.r = {1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0};
     mCameraInfoMessage.k = {
-        focalLength, 0.0, (double)mRangeFinder->getWidth() / 2,
-        0.0, focalLength, (double)mRangeFinder->getHeight() / 2,
+        focalLengthx, 0.0, (double)width / 2,
+        0.0, focalLengthy, (double)height / 2,
         0.0, 0.0, 1.0};
     mCameraInfoMessage.p = {
-        focalLength, 0.0, (double)mRangeFinder->getWidth() / 2, 0.0,
-        0.0, focalLength, (double)mRangeFinder->getHeight() / 2, 0.0,
+        focalLengthx, 0.0, (double)width / 2, 0.0,
+        0.0, focalLengthy, (double)height / 2, 0.0,
         0.0, 0.0, 1.0, 0.0};
     mCameraInfoPublisher->publish(mCameraInfoMessage);
 
     // Point cloud publisher
     mPointCloudPublisher = mNode->create_publisher<sensor_msgs::msg::PointCloud2>(mTopicName + "/point_cloud", rclcpp::SensorDataQoS().reliable());
     mPointCloudMessage.header.frame_id = mFrameName;
-    mPointCloudMessage.is_dense = false;
     mPointCloudMessage.fields.resize(3);
     mPointCloudMessage.fields[0].name = "x";
+    mPointCloudMessage.fields[0].offset = 0;
     mPointCloudMessage.fields[0].datatype = sensor_msgs::msg::PointField::FLOAT32;
     mPointCloudMessage.fields[0].count = 1;
-    mPointCloudMessage.fields[0].offset = 0;
     mPointCloudMessage.fields[1].name = "y";
+    mPointCloudMessage.fields[1].offset = 4;
     mPointCloudMessage.fields[1].datatype = sensor_msgs::msg::PointField::FLOAT32;
     mPointCloudMessage.fields[1].count = 1;
-    mPointCloudMessage.fields[1].offset = 4;
     mPointCloudMessage.fields[2].name = "z";
+    mPointCloudMessage.fields[2].offset = 8;
     mPointCloudMessage.fields[2].datatype = sensor_msgs::msg::PointField::FLOAT32;
     mPointCloudMessage.fields[2].count = 1;
-    mPointCloudMessage.fields[2].offset = 8;
     mPointCloudMessage.is_bigendian = false;
-    const int width = mRangeFinder->getWidth();
-    const int height = mRangeFinder->getHeight();
     mPointCloudMessage.width = width;
     mPointCloudMessage.height = height;
-    mPointCloudMessage.point_step = 12;
-    mPointCloudMessage.row_step = width * 12;
-    mPointCloudMessage.data.resize(width * height * 12);
+    mPointCloudMessage.point_step = 4;
+    mPointCloudMessage.row_step = width * 4;
+    mPointCloudMessage.data.resize(width * 4 * height);
 
     if (mAlwaysOn) {
       mRangeFinder->enable(mPublishTimestepSyncedMs);
@@ -98,11 +99,10 @@ namespace webots_ros2_driver
     if (!preStep())
       return;
 
-    if (mIsEnabled)
+    if (mIsEnabled) {
       publishImage();
-    
-    if (mIsEnabled)
       publishPointCloud();
+    }
 
     if (mAlwaysOn)
       return;
@@ -130,6 +130,7 @@ namespace webots_ros2_driver
     }
   }
 
+  // To be redesigned when mRangeFinder->getPointCloud() will be implemented on Webots side.
   void Ros2RangeFinder::publishPointCloud()
   {
     auto image = mRangeFinder->getRangeImage();
@@ -144,15 +145,18 @@ namespace webots_ros2_driver
       const float fx = mCameraInfoMessage.k[0];
       const float fy = mCameraInfoMessage.k[4];
 
+      int idx;
+      float x, y, z;
+
       float* data = (float*)mPointCloudMessage.data.data();
       for (int j = 0; j < height; j++)
       {
         for (int i = 0; i < width; i++)
         {
-          int idx = i + j * width;
-          float x = image[idx];
-          float y = -(i - cx) * x / fx;
-          float z = -(j - cy) * x / fy;
+          idx = i + j * width;
+          x = image[idx];
+          y = -(i - cx) * x / fx;
+          z = -(j - cy) * x / fy;
           memcpy(data + idx * 3    , &x, sizeof(float));
           memcpy(data + idx * 3 + 1, &y, sizeof(float));
           memcpy(data + idx * 3 + 2, &z, sizeof(float));

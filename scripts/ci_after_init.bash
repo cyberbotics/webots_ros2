263bd3b55c562ce5385c6cc9dc2429fc92d982b6
ad-daniel <daniel.dias@epfl.ch>
2022-05-31 17:02:20 +0200

    Apply openssl patch

--- scripts/ci_after_init.bash
+++ scripts/ci_after_init.bash
@@ -14,9 +14,17 @@ NIGHTLY_DATE=`date --date="${LAST_NIGHTLY_DAY_OLD} day ago" +"%-d_%-m_%Y"`
 WEBOTS_NIGHTLY_VERSION="nightly_${NIGHTLY_DATE}"
 
 apt update
-apt install -y wget dialog apt-utils psmisc
+apt install -y wget dialog apt-utils psmisc lsb-release
 wget https://github.com/cyberbotics/webots/releases/download/${WEBOTS_NIGHTLY_VERSION}/webots_${WEBOTS_RELEASE_VERSION}_amd64.deb -O /tmp/webots.deb
 apt install -y /tmp/webots.deb xvfb
+
+# OpenSSL patch for ubuntu 22
+UBUNTU_VERSION=$(lsb_release -rs)
+if [[ $UBUNTU_VERSION == "22.04" ]]; then
+  wget https://cyberbotics.com/files/repository/dependencies/linux64/release/libssl_1.1.tar.xz
+  tar xvf libssl_1.1.tar.xz
+  sudo mv openssl-1.1/* /usr/local/webots/lib/webots/
+fi
 
 # The following packages are only available in the ROS 2 Foxy/Galactic distributions. Therefore, we cannot include them in the package.xml, but we have to install them manually here.
 

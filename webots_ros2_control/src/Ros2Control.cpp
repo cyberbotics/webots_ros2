ed45f7410c48a725769eef30e4c11e5696cf2f93
ad-daniel <44834743+ad-daniel@users.noreply.github.com>
2022-06-01 02:01:53 +0200

    Revert

--- webots_ros2_control/src/Ros2Control.cpp
+++ webots_ros2_control/src/Ros2Control.cpp
@@ -103,7 +103,7 @@ namespace webots_ros2_control
       resourceManager->import_component(std::move(webotsSystem), controlHardware[i]);
 #endif
 
-#if HUMBLE
+#if HUMBLE || ROLLING
       resourceManager->activate_all_components();
 #endif
     }

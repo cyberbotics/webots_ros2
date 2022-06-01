fe7f05984395250e66ed97a33072e520a3342497
ad-daniel <daniel.dias@epfl.ch>
2022-06-01 18:49:00 +0200

    Review fix

--- webots_ros2_epuck/launch/robot_launch.py
+++ webots_ros2_epuck/launch/robot_launch.py
@@ -68,7 +68,8 @@ def generate_launch_description():
 
     mappings = [('/diffdrive_controller/cmd_vel_unstamped', '/cmd_vel')]
     if ('ROS_DISTRO' in os.environ and os.environ['ROS_DISTRO'] == 'rolling') or \
-       ('ROS_REPO' in os.environ and os.environ['ROS_REPO'] == 'testing'):
+       ('ROS_DISTRO' in os.environ and os.environ['ROS_DISTRO'] == 'humble' and
+       'ROS_REPO' in os.environ and os.environ['ROS_REPO'] == 'testing'):
         mappings.append(('/diffdrive_controller/odom', '/odom'))
 
     epuck_driver = Node(

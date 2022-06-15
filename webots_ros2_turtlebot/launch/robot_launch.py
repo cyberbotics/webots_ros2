5b59d0b1fbeff4c3f75a447bd152c10853f4691b
ad-daniel <44834743+ad-daniel@users.noreply.github.com>
2022-06-15 16:43:25 +0200

    Add nightly tests (#453)
    * Add nightly tests
    
    * Fix humble after sync
    
    * Make develop tests only nightly
    
    * Sync
    
    * Fix test

--- webots_ros2_turtlebot/launch/robot_launch.py
+++ webots_ros2_turtlebot/launch/robot_launch.py
@@ -62,8 +62,7 @@ def generate_launch_description():
     )
 
     mappings = [('/diffdrive_controller/cmd_vel_unstamped', '/cmd_vel')]
-    if ('ROS_DISTRO' in os.environ and os.environ['ROS_DISTRO'] == 'rolling') or \
-       ('ROS_REPO' in os.environ and os.environ['ROS_REPO'] == 'testing'):
+    if 'ROS_DISTRO' in os.environ and os.environ['ROS_DISTRO'] in ['humble', 'rolling']:
         mappings.append(('/diffdrive_controller/odom', '/odom'))
 
     turtlebot_driver = Node(

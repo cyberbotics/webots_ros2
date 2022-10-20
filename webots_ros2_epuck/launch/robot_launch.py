48cb64bc1a9d2337f6f19f2ad2865ea72798db74
Yannick Goumaz <yannick.goumaz@epfl.ch>
2022-10-20 11:03:49 +0200

    restore webots kill on shutdown

--- webots_ros2_epuck/launch/robot_launch.py
+++ webots_ros2_epuck/launch/robot_launch.py
@@ -132,13 +132,13 @@ def generate_launch_description():
         robot_state_publisher,
         epuck_driver,
         footprint_publisher,
-        epuck_process
+        epuck_process,
 
-        #This action will kill all nodes once the Webots simulation has exited
-        #launch.actions.RegisterEventHandler(
-        #    event_handler=launch.event_handlers.OnProcessExit(
-        #        target_action=webots,
-        #        on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
-        #    )
-        #)
+        # This action will kill all nodes once the Webots simulation has exited
+        launch.actions.RegisterEventHandler(
+            event_handler=launch.event_handlers.OnProcessExit(
+                target_action=webots,
+                on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
+            )
+        )
     ])

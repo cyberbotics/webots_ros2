554a293d77bc9c7f17ebc5f93d824fd077c814d3
Yannick Goumaz <yannick.goumaz@epfl.ch>
2022-10-24 17:07:11 +0200

    Keep ROS client alive

--- webots_ros2_driver/webots_ros2_driver/utils.py
+++ webots_ros2_driver/webots_ros2_driver/utils.py
@@ -126,12 +126,12 @@ def container_shared_folder():
 
 
 def connect_to_host():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        while s.connect_ex(('host.docker.internal', 2000)) != 0:
-            print('WARNING: Unable to start Webots. Please start the local simulation server on your host machine. Next connection attempt in 1 second.', file=sys.stderr)
-            time.sleep(1)
-        s.sendall(host_shared_folder().encode('utf-8'))
-        data = s.recv(1024)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    while s.connect_ex(('host.docker.internal', 2000)) != 0:
+        print('WARNING: Unable to start Webots. Please start the local simulation server on your host machine. Next connection attempt in 1 second.', file=sys.stderr)
+        time.sleep(1)
+    s.sendall(host_shared_folder().encode('utf-8'))
+    data = s.recv(1024)
     return data.decode('utf-8')
 
 

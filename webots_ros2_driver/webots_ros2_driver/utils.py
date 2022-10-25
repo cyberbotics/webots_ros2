5db6a0eb77e226c5fd28f51a33a02a83ae3ec73d
Yannick Goumaz <yannick.goumaz@epfl.ch>
2022-10-25 10:49:33 +0200

    global socket to keep connection alive

--- webots_ros2_driver/webots_ros2_driver/utils.py
+++ webots_ros2_driver/webots_ros2_driver/utils.py
@@ -126,6 +126,7 @@ def container_shared_folder():
 
 
 def connect_to_host():
+    global s
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     while s.connect_ex(('host.docker.internal', 2000)) != 0:
         print('WARNING: Unable to start Webots. Please start the local simulation server on your host machine. Next connection attempt in 1 second.', file=sys.stderr)

d78b481c36cc38daf7ee6e3c1549eda16853232e
Yannick Goumaz <61198661+ygoumaz@users.noreply.github.com>
2022-11-25 17:33:28 +0100

    Enhancement add auto libcontroller sync (#528)
    * cleanup + new script
    
    * launch test
    
    * fix wrong path
    
    * Update titles and url
    
    * Copy files anyway
    
    * improve infos and copy
    
    * remove comments
    
    * final touch
    
    * change step name
    
    * trigger sync
    
    * change checkout branch
    
    * restore changes

--- webots_ros2_driver/webots/lib/controller/matlab/wb_robot_init.m
+++
@@ -1,4 +0,0 @@
-function wb_robot_init()
-% Usage: wb_robot_init()
-% Matlab API for Webots
-% This is a dummy function: it does nothing
--- webots_ros2_driver/webots/lib/controller/matlab/wb_skin_get_bone_position.m
+++
@@ -1,7 +0,0 @@
-function result = wb_skin_get_bone_position(tag, index, absolute)
-% Usage: wb_skin_get_bone_position(tag, index, absolute)
-% Matlab API for Webots
-
-obj = calllib('libController', 'wb_skin_get_bone_position', tag, index, absolute);
-setdatatype(obj,'doublePtr', 1, 3);
-result = get(obj, 'Value');

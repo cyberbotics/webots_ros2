564ba94b44f53ba0b688a340dcaf26d6d954250e
Yannick Goumaz <61198661+ygoumaz@users.noreply.github.com>
2022-11-30 17:05:34 +0100

    Prepare for version 2023.0.0 (#545)
    Co-authored-by: ad-daniel <44834743+ad-daniel@users.noreply.github.com>

--- webots_ros2_tests/setup.py
+++ webots_ros2_tests/setup.py
@@ -11,7 +11,7 @@ data_files = [
 
 setup(
     name=package_name,
-    version='2022.1.4',
+    version='2023.0.0',
     packages=[package_name],
     data_files=data_files,
     install_requires=['setuptools', 'launch'],

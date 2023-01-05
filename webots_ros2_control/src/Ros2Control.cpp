db38ed564910a08413169c308013a6b726913653
ad-daniel <44834743+ad-daniel@users.noreply.github.com>
2023-01-05 10:21:58 +0100

    Improve comparison (#590)
    * Improve
    
    * Minor
    
    * Fix

--- webots_ros2_control/src/Ros2Control.cpp
+++ webots_ros2_control/src/Ros2Control.cpp
@@ -96,7 +96,8 @@ namespace webots_ros2_control
     for (unsigned int i = 0; i < controlHardware.size(); i++)
     {
 
-#if HARDWARE_INTERFACE_VARIABLE_RENAMING
+// Necessary hotfix for renamed variables present in "hardware_interface" package for versions above 3.5 (#590)
+#if HARDWARE_INTERFACE_VERSION_MAJOR >= 3 && HARDWARE_INTERFACE_VERSION_MINOR >= 5
       const std::string pluginName = controlHardware[i].hardware_plugin_name;
       auto webotsSystem = std::unique_ptr<webots_ros2_control::Ros2ControlSystemInterface>(
           mHardwareLoader->createUnmanagedInstance(pluginName));

68375039da7924551584b23ead1f6dcb92d8dbf4
Yannick Goumaz <yannick.goumaz@epfl.ch>
2023-03-13 14:49:49 +0100

    cyclone only on Humble

--- scripts/ci_before_init_embed.bash
+++ scripts/ci_before_init_embed.bash
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+ROS_DISTRO=$1
+
 export WEBOTS_RELEASE_VERSION=2023a-rev1
 export WEBOTS_OFFSCREEN=1
 export CI=1
 export DEBIAN_FRONTEND=noninteractive
-export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+if [[ "${ROS_DISTRO}" == "humble" ]]; then
+    export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+fi

ba3d9386fd57a69904849fc96429a7a4e8e0ab80
Angel Ayala <angel4ayala@gmail.com>
2023-03-24 17:32:47 -0300

    Including @lukicdarkoo and @ygoumaz comments

--- webots_ros2_driver/include/webots_ros2_driver/plugins/static/Ros2Compass.hpp
+++ webots_ros2_driver/include/webots_ros2_driver/plugins/static/Ros2Compass.hpp
@@ -4,11 +4,8 @@
 #include <webots_ros2_driver/WebotsNode.hpp>
 #include <webots_ros2_driver/plugins/Ros2SensorPlugin.hpp>
 
-#include <sensor_msgs/msg/magnetic_field.hpp>
-#include <webots_ros2_msgs/srv/get_bool.hpp>
-#include <webots_ros2_msgs/srv/get_float_array.hpp>
-#include <webots_ros2_msgs/srv/get_int.hpp>
-#include <webots_ros2_msgs/srv/set_int.hpp>
+#include <geometry_msgs/msg/vector3_stamped.hpp>
+#include <webots_ros2_msgs/msg/float_stamped.hpp>
 
 namespace webots_ros2_driver {
   class Ros2Compass : public Ros2SensorPlugin {
@@ -20,22 +17,12 @@ namespace webots_ros2_driver {
     bool mustPublish();
     void publishData();
 
-    void enable_callback(const std::shared_ptr<webots_ros2_msgs::srv::SetInt::Request> request,
-                         std::shared_ptr<webots_ros2_msgs::srv::SetInt::Response> response);
-    void disable_callback(const std::shared_ptr<webots_ros2_msgs::srv::GetBool::Request> request,
-                          std::shared_ptr<webots_ros2_msgs::srv::GetBool::Response> response);
-    void get_sampling_period_callback(const std::shared_ptr<webots_ros2_msgs::srv::GetInt::Request> request,
-                                      std::shared_ptr<webots_ros2_msgs::srv::GetInt::Response> response);
-    void get_lookup_table_callback(const std::shared_ptr<webots_ros2_msgs::srv::GetFloatArray::Request> request,
-                                   std::shared_ptr<webots_ros2_msgs::srv::GetFloatArray::Response> response);
     // ROS2 topics
-    rclcpp::Publisher<sensor_msgs::msg::MagneticField>::SharedPtr mDataPublisher;
-    sensor_msgs::msg::MagneticField mDataMessage;
+    rclcpp::Publisher<sensor_msgs::msg::Vector3Stamped>::SharedPtr mVectorPublisher;
+    geometry_msgs::msg::Vector3Stamped mVectorMessage;
+    rclcpp::Publisher<webots_ros2_msgs::msg::Float64Stamped>::SharedPtr mFloatPublisher;
+    webots_ros2_msgs::msg::Float64Stamped mFloatMessage;
 
-    rclcpp::Service<webots_ros2_msgs::srv::SetInt>::SharedPtr mEnableService;
-    rclcpp::Service<webots_ros2_msgs::srv::GetBool>::SharedPtr mDisableService;
-    rclcpp::Service<webots_ros2_msgs::srv::GetInt>::SharedPtr mGetSamplingPeriodService;
-    rclcpp::Service<webots_ros2_msgs::srv::GetFloatArray>::SharedPtr mGetLookupTableService;
     // Device
     WbDeviceTag mCompass;
     // Runtime vars
--- webots_ros2_driver/src/plugins/static/Ros2Compass.cpp
+++ webots_ros2_driver/src/plugins/static/Ros2Compass.cpp
@@ -18,19 +18,12 @@ namespace webots_ros2_driver {
     assert(mCompass != 0);
 
     // Data publisher
-    mDataPublisher =
-      mNode->create_publisher<sensor_msgs::msg::MagneticField>(mTopicName + "/values", rclcpp::SensorDataQoS().reliable());
-    // Services
-    mEnableService = mNode->create_service<webots_ros2_msgs::srv::SetInt>(
-      mTopicName + "/enable", std::bind(&Ros2Compass::enable_callback, this, _1, _2));
-    mDisableService = mNode->create_service<webots_ros2_msgs::srv::GetBool>(
-      mTopicName + "/disable", std::bind(&Ros2Compass::disable_callback, this, _1, _2));
-    mGetSamplingPeriodService = mNode->create_service<webots_ros2_msgs::srv::GetInt>(
-      mTopicName + "/get_sampling_period", std::bind(&Ros2Compass::get_sampling_period_callback, this, _1, _2));
-    mGetLookupTableService = mNode->create_service<webots_ros2_msgs::srv::GetFloatArray>(
-      mTopicName + "/get_lookup_table", std::bind(&Ros2Compass::get_lookup_table_callback, this, _1, _2));
-    RCLCPP_DEBUG(rclcpp::get_logger(mDeviceName), (mDeviceName + " initialized!").c_str());
+    mVectorPublisher =
+      mNode->create_publisher<geometry_msgs::msg::Vector3Stamped>(mTopicName + "/values", rclcpp::SensorDataQoS().reliable());
+    mVectorPublisher =
+      mNode->create_publisher<webots_ro2_msgs::msg::FloatStamped>(mTopicName + "/bearing", rclcpp::SensorDataQoS().reliable());
 
+    RCLCPP_DEBUG(rclcpp::get_logger(mDeviceName), (mDeviceName + " initialized!").c_str());
     // Calculate timestep
     if (mAlwaysOn) {
       wb_compass_enable(mCompass, mPublishTimestepSyncedMs);
@@ -63,42 +56,21 @@ namespace webots_ros2_driver {
     return mIsEnabled;
   }
   void Ros2Compass::publishData() {
-    const double *compass_values = wb_compass_get_values(mCompass);
-    mDataMessage.header.stamp = mNode->get_clock()->now();
-    mDataMessage.magnetic_field.x = compass_values[0];
-    mDataMessage.magnetic_field.y = compass_values[1];
-    mDataMessage.magnetic_field.z = compass_values[2];
-    mDataPublisher->publish(mDataMessage);
-  }
-  void Ros2Compass::enable_callback(const std::shared_ptr<webots_ros2_msgs::srv::SetInt::Request> request,
-                                    std::shared_ptr<webots_ros2_msgs::srv::SetInt::Response> response) {
-    wb_compass_enable(mCompass, request->value);
-    mAlwaysOn = true;
-    mIsEnabled = true;
-    response->success = true;
-  }
-  void Ros2Compass::disable_callback(const std::shared_ptr<webots_ros2_msgs::srv::GetBool::Request> request,
-                                     std::shared_ptr<webots_ros2_msgs::srv::GetBool::Response> response) {
-    wb_compass_disable(mCompass);
-    mAlwaysOn = false;
-    mIsEnabled = false;
-    response->value = true;
-  }
-  void Ros2Compass::get_sampling_period_callback(const std::shared_ptr<webots_ros2_msgs::srv::GetInt::Request> request,
-                                                 std::shared_ptr<webots_ros2_msgs::srv::GetInt::Response> response) {
-    response->value = wb_compass_get_sampling_period(mCompass);
-  }
-  void Ros2Compass::get_lookup_table_callback(const std::shared_ptr<webots_ros2_msgs::srv::GetFloatArray::Request> request,
-                                              std::shared_ptr<webots_ros2_msgs::srv::GetFloatArray::Response> response) {
-    int table_size = wb_compass_get_lookup_table_size(mCompass);
-    std::vector<double> values(table_size * 3);
-    const double *lookup_table = wb_compass_get_lookup_table(mCompass);
-    for (int i = 0; i < table_size; i++) {
-      int offset = i * 3;
-      values.at(offset) = lookup_table[offset];
-      values.at(offset + 1) = lookup_table[offset + 1];
-      values.at(offset + 2) = lookup_table[offset + 2];
-    }
-    response->values = values;
+    const double *north_vector = wb_compass_get_values(mCompass);
+    mVectorMessage.header.stamp = mNode->get_clock()->now();
+    mVectorMessage.vector.x = north_vector[0];
+    mVectorMessage.vector.y = north_vector[1];
+    mVectorMessage.vector.z = north_vector[2];
+    mVectorPublisher->publish(mVectorMessage);
+    
+    // north degree
+    double rad = atan2(north_vector[1], north_vector[0]);
+    double bearing = (rad - 1.5708) / M_PI * 180.0;
+    if (bearing < 0.0)
+        bearing = bearing + 360.0;
+
+    mFloatMessage.header.stamp = mVectorMessage.header.stamp
+    mFloatMessage.data = bearing;
+    mVectorPublisher->publish(mFloatMessage);
   }
 }  // namespace webots_ros2_driver

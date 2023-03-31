b7eb6bc63043c16eaf5c247eebceac5a780f21d5
Yannick Goumaz <yannick.goumaz@epfl.ch>
2023-03-31 16:24:04 +0200

    minor clean

--- webots_ros2_epuck/launch/robot_tools_launch.py
+++ webots_ros2_epuck/launch/robot_tools_launch.py
@@ -24,7 +24,6 @@ from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory, get_packages_with_prefixes
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import ExecuteProcess
 
 
 def generate_launch_description():

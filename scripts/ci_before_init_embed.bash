8523bc013058a641d7093eceadb6dd60f3eb912e
Yannick Goumaz <yannick.goumaz@epfl.ch>
2023-04-11 16:32:40 +0200

    Add target ws

--- scripts/ci_before_init_embed.bash
+++ scripts/ci_before_init_embed.bash
@@ -6,6 +6,7 @@ export WEBOTS_RELEASE_VERSION=2023a-rev1
 export WEBOTS_OFFSCREEN=1
 export CI=1
 export DEBIAN_FRONTEND=noninteractive
+export UNDERLAY=/root/target_ws/install
 
 # TODO: Revert once the https://github.com/ros-planning/navigation2/issues/3033 issue is fixed.
 # Fast-DDS is not working properly with the Nav2 package on Humble. Using Cyclone DDS instead.

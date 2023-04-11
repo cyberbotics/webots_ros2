2738d0219142a3473d4a1c7f833721bb1abacd6b
Yannick Goumaz <yannick.goumaz@epfl.ch>
2023-04-11 14:59:18 +0200

    install git

--- scripts/ci_after_init.bash
+++ scripts/ci_after_init.bash
@@ -28,7 +28,7 @@ fi
 
 # The following packages are not available in the ROS 2 Rolling distribution. Therefore, we cannot include them in the package.xml, but we have to install them manually here.
 if [[ "${ROS_DISTRO}" != "rolling" ]]; then
-    apt install -y ros-${ROS_DISTRO}-nav2-bringup
+    apt install -y ros-${ROS_DISTRO}-nav2-bringup git
     git clone -b ${ROS_DISTRO}-devel https://github.com/ROBOTIS-GIT/turtlebot3.git /root/target_ws/src
 fi
 

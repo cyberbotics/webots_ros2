b15b44a6f923454beccc94b9edb104c05d97d59d
Yannick Goumaz <61198661+ygoumaz@users.noreply.github.com>
2023-04-12 10:52:24 +0200

    Fix CI tests (#706)
    * install from turtlebot3 from sources
    
    * install git
    
    * Add target ws
    
    * separate ws for turtlebot
    
    * cleanup + fix ros not found
    
    * try upstream ws
    
    * ignore rviz in CI
    
    * fix duplicate name
    
    * clone fork
    
    * remove use rviz from launch file
    
    * cleanup + comment

--- scripts/ci_before_init_embed.bash
+++ scripts/ci_before_init_embed.bash
@@ -6,6 +6,11 @@ export WEBOTS_RELEASE_VERSION=2023a-rev1
 export WEBOTS_OFFSCREEN=1
 export CI=1
 export DEBIAN_FRONTEND=noninteractive
+# The upstream workspace allows to install packages from sources for the tests (Turtlebot3 here)
+if [[ "${ROS_DISTRO}" != "rolling" ]]; then
+    export UPSTREAM_WORKSPACE=/root/turtlebot_ws/
+fi
+
 
 # TODO: Revert once the https://github.com/ros-planning/navigation2/issues/3033 issue is fixed.
 # Fast-DDS is not working properly with the Nav2 package on Humble. Using Cyclone DDS instead.

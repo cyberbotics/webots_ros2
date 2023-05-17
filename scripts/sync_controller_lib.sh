fd4110fbcd2f11391f15aa0c757e5fdb4f2f525e
Yannick Goumaz <61198661+ygoumaz@users.noreply.github.com>
2023-05-17 09:32:17 +0200

    Update sync_controller_lib.sh

--- scripts/sync_controller_lib.sh
+++ scripts/sync_controller_lib.sh
@@ -60,5 +60,5 @@ mkdir -p src/stb
 cp -r ${WEBOTS_HOME}/src/controller/* src/controller
 cp -r ${WEBOTS_HOME}/src/stb/* src/stb
 
-rm -rf ${WEBOTS_HOME}/.gitignore
+rm -rf .gitignore
 cp ${WEBOTS_HOME}/.gitignore .gitignore

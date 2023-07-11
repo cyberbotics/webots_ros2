56693985d63fb7054a90afae0b6370d472465549
Jakub Delicat <jakub.delicat@husarion.com>
2023-07-11 13:21:49 +0200

    inf or nan scan implementation
    Signed-off-by: Jakub Delicat <jakub.delicat@husarion.com>

--- webots_ros2_tests/test/test_system_rosbot_xl.py
+++ webots_ros2_tests/test/test_system_rosbot_xl.py
@@ -70,9 +70,6 @@ class TestROSbotXL(TestWebots):
         publisher = self.__node.create_publisher(Twist, '/cmd_vel', 1)
 
         def on_position_message_received(message):
-            print(message.header)
-            print(message.pose.pose)
-
             twist_message = Twist()
             twist_message.linear.x = 0.5
             twist_message.angular.z = 0.3
@@ -87,17 +84,14 @@ class TestROSbotXL(TestWebots):
 
     def testScan(self):
         def on_scan_message_received(message):
-            print(message.header)
-            print(message.ranges)
             # There should be at least 1 range bigger than 0 and some = 0
             number_of_inf = 0
             number_of_non_zeroes = 0
             for value in message.ranges:
-                if value == float('inf'):
+                if value == float('inf') or value == float('nan'):
                     number_of_inf += 1
                 elif value > 0.:
                     number_of_non_zeroes += 1
-            print(f'number_of_inf: {number_of_inf}, number_of_non_zeroes: {number_of_non_zeroes}')
             return number_of_inf > 0 and number_of_non_zeroes > 0
 
         self.wait_for_messages(self.__node, LaserScan, '/scan_filtered', condition=on_scan_message_received)

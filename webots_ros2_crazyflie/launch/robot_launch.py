cfb4a99579832f6e2c85941d6750410b47edb282
knmcguire <kimberly@bitcraze.io>
2023-12-07 17:01:24 +0100

    Added crazyflie ros2 webots package

---
+++ webots_ros2_crazyflie/launch/robot_launch.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+# Copyright 1996-2023 Cyberbotics Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Launch Webots Crazyflie driver."""
+
+import os
+import launch
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions.path_join_substitution import PathJoinSubstitution
+from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from webots_ros2_driver.webots_launcher import WebotsLauncher
+from webots_ros2_driver.webots_controller import WebotsController
+
+
+def generate_launch_description():
+    package_dir = get_package_share_directory('webots_ros2_crazyflie')
+    world = LaunchConfiguration('world')
+
+    webots = WebotsLauncher(
+        world=PathJoinSubstitution([package_dir, 'worlds', world]),
+        ros2_supervisor=True
+    )
+
+    robot_description_path = os.path.join(package_dir, 'resource', 'crazyflie_webots.urdf')
+    crazyflie_driver = WebotsController(
+        robot_name='Crazyflie',
+        parameters=[
+            {'robot_description': robot_description_path},
+        ],
+        respawn=True
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'world',
+            default_value='crazyflie_apartment.wbt',
+            description='Choose one of the world files from `/webots_ros2_crazyflie/worlds` directory'
+        ),
+        webots,
+        webots._supervisor,
+        crazyflie_driver,
+
+        # This action will kill all nodes once the Webots simulation has exited
+        launch.actions.RegisterEventHandler(
+            event_handler=launch.event_handlers.OnProcessExit(
+                target_action=webots,
+                on_exit=[
+                    launch.actions.EmitEvent(event=launch.events.Shutdown())
+                ],
+            )
+        )
+    ])

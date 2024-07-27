7948fcfe186505f0850d53ff8e3aee0967b703a0
Darko Lukic <lukicdarkoo@gmail.com>
2024-07-27 22:36:38 +0200

    ci

--- scripts/ci_after_init.bash
+++ scripts/ci_after_init.bash
@@ -53,5 +53,5 @@ fi
 #   MESA: error: ZINK: failed to choose pdev
 #   2024-07-27T19:23:20.1063344Z [webots-4] glx: failed to create drisw screen
 if [[ "${ROS_DISTRO}" == "rolling" ]]; then
-    apt-get install -y qml-module-qtquick-controls
+    apt-get install -y gqrx-sdr
 fi

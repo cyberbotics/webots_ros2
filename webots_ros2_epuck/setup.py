6a661bdb8a4247b52b18bbff71cb40694874a8c9
Darko Lukic <lukicdarkoo@gmail.com>
2024-12-27 15:44:13 +0100

    version increase

--- webots_ros2_epuck/setup.py
+++ webots_ros2_epuck/setup.py
@@ -43,7 +43,7 @@ data_files.append(('share/' + package_name, [
 
 setup(
     name=package_name,
-    version='2023.1.3',
+    version='2025.0.0',
     packages=[package_name],
     data_files=data_files,
     install_requires=['setuptools', 'launch'],
